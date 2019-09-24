8edb2a193f8f91b585b1a4ab7aa32dad8afb7444
Makentoshe <mkliverout@gmail.com>
2019-09-24 15:46:00 +0300

    [test] Add tests for GetPostsBySearch

--- app/src/test/java/com/makentoshe/habrachan/common/model/network/GetPostsBySearchTest.kt
+++ app/src/test/java/com/makentoshe/habrachan/common/model/network/GetPostsBySearchTest.kt
@@ -1,5 +1,57 @@
 package com.makentoshe.habrachan.common.model.network
 
-import org.junit.Assert.*
+import com.makentoshe.habrachan.common.model.entity.GetPostsBySearchResult
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+import retrofit2.Call
+import retrofit2.Response
 
-class GetPostsBySearchTest
+class GetPostsBySearchTest {
+
+    @Test
+    fun `should return a result`() {
+        val result: GetPostsBySearchResult = mockk()
+
+        val response: Response<GetPostsBySearchResult> = mockk()
+        every { response.body() } returns result
+
+        val call: Call<GetPostsBySearchResult> = mockk()
+        every { call.execute() } returns response
+
+        val habrApi: HabrApi = mockk()
+        every { habrApi.getPostsBySearch(any(), any(), any(), any()) } returns call
+
+        GetPostsBySearch(habrApi).execute(GetPostsBySearchRequest()).also {
+            assertEquals(result, it)
+        }
+    }
+
+    @Test
+    fun `should return a result with error`() {
+        val code = 21345
+        val message = "asdfghkjl;htr"
+
+        val response: Response<GetPostsBySearchResult> = mockk()
+        every { response.body() } returns null
+        every { response.code() } returns code
+        every { response.message() } returns message
+
+        val call: Call<GetPostsBySearchResult> = mockk()
+        every { call.execute() } returns response
+
+        val habrApi: HabrApi = mockk()
+        every { habrApi.getPostsBySearch(any(), any(), any(), any()) } returns call
+
+        GetPostsBySearch(habrApi).execute(GetPostsBySearchRequest()).also {
+            assertEquals(false, it.success)
+            assertEquals(code, it.data.code)
+            assertEquals(message, it.data.message)
+            assertNull(it.data.articles)
+            assertNull(it.data.data)
+            assertNull(it.data.pages)
+        }
+    }
+}
