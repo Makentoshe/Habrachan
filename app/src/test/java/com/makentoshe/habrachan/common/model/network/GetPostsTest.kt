248a70f40e1757bc563a73884adb432a6856fd38
Makentoshe <mkliverout@gmail.com>
2019-09-25 23:32:04 +0300

    [test] Add test for posts getting

---
+++ app/src/test/java/com/makentoshe/habrachan/common/model/network/GetPostsTest.kt
@@ -0,0 +1,57 @@
+package com.makentoshe.habrachan.common.model.network
+
+import com.makentoshe.habrachan.common.model.network.posts.*
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+import retrofit2.Call
+import retrofit2.Response
+
+class GetPostsTest {
+
+    @Test
+    fun `should return a result`() {
+        val result: GetPostsResult = mockk()
+
+        val response: Response<GetPostsResult> = mockk()
+        every { response.body() } returns result
+
+        val call: Call<GetPostsResult> = mockk()
+        every { call.execute() } returns response
+
+        val habrApi: HabrApi = mockk()
+        every { habrApi.getPosts(allAny(), any()) } returns call
+
+        GetPosts(habrApi).execute(GetPostsRequest()).also {
+            assertEquals(result, it)
+        }
+    }
+
+    @Test
+    fun `should return a result with error`() {
+        val code = 21345
+        val message = "asdfghkjl;htr"
+
+        val response: Response<GetPostsResult> = mockk()
+        every { response.body() } returns null
+        every { response.code() } returns code
+        every { response.message() } returns message
+
+        val call: Call<GetPostsResult> = mockk()
+        every { call.execute() } returns response
+
+        val habrApi: HabrApi = mockk()
+        every { habrApi.getPosts(allAny(), any()) } returns call
+
+        GetPosts(habrApi).execute(GetPostsRequest()).also {
+            assertEquals(false, it.success)
+            assertEquals(code, it.data.code)
+            assertEquals(message, it.data.message)
+            assertNull(it.data.articles)
+            assertNull(it.data.data)
+            assertNull(it.data.pages)
+        }
+    }
+}
