541b86dd50f91111753f3bc91966323d5b87631d
Makentoshe <mkliverout@gmail.com>
2019-09-25 23:33:42 +0300

    [refactor] Add default value for page

--- app/src/main/java/com/makentoshe/habrachan/common/model/network/posts/GetPostsRequest.kt
+++ app/src/main/java/com/makentoshe/habrachan/common/model/network/posts/GetPostsRequest.kt
@@ -1,7 +1,7 @@
 package com.makentoshe.habrachan.common.model.network.posts
 
 data class GetPostsRequest(
-    val page: Int,
+    val page: Int = 1,
     val sort: String? = null,
     val hl: String? = null,
     val fl: String? = null,
