40e9a740bd1f426c0d764710a2aa1af1ef9a84d1
Makentoshe <mkliverout@gmail.com>
2019-09-26 21:38:13 +0300

    [feature] Add conventory factories

--- app/src/main/java/com/makentoshe/habrachan/common/model/network/posts/PostsConverterFactory.kt
+++ app/src/main/java/com/makentoshe/habrachan/common/model/network/posts/PostsConverterFactory.kt
@@ -6,10 +6,44 @@ import retrofit2.Converter
 import retrofit2.Retrofit
 import java.lang.reflect.Type
 
-class PostsConverterFactory: Converter.Factory() {
+class PostsConverterFactory : Converter.Factory() {
+
+    private val converter by lazy {
+        Converter<ResponseBody, GetPostsResult> {
+            Gson().fromJson(it.string(), GetPostsResult::class.java)
+        }
+    }
+
     override fun responseBodyConverter(
         type: Type, annotations: Array<Annotation>, retrofit: Retrofit
-    ) = Converter<ResponseBody, GetPostsResult> {
-        Gson().fromJson(it.string(), GetPostsResult::class.java)
+    ): Converter<ResponseBody, GetPostsResult>? {
+        return if (type == GetPostsResult::class.java) converter else null
+    }
+
+}
+
+class StringConverterFactory: Converter.Factory() {
+
+    private val converter by lazy {
+        Converter<ResponseBody, String> { it.string() }
+    }
+
+    override fun responseBodyConverter(
+        type: Type, annotations: Array<Annotation>, retrofit: Retrofit
+    ): Converter<ResponseBody, String>? {
+        return if (type == String::class.java) converter else null
+    }
+}
+
+class ByteArrayConverterFactory: Converter.Factory() {
+
+    private val converter by lazy {
+        Converter<ResponseBody, ByteArray> { it.bytes() }
+    }
+
+    override fun responseBodyConverter(
+        type: Type, annotations: Array<Annotation>, retrofit: Retrofit
+    ): Converter<ResponseBody, ByteArray>? {
+        return if (type.toString() == "byte[]" || type == ByteArray::class.java) converter else null
     }
 }
