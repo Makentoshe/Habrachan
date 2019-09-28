cb89d5ee983f3072adb5ead7f8ff638248420bd8
Makentoshe <mkliverout@gmail.com>
2019-09-29 01:11:22 +0300

    [feature] Add result sealed class

---
+++ app/src/main/java/com/makentoshe/habrachan/common/model/network/Result.kt
@@ -0,0 +1,17 @@
+package com.makentoshe.habrachan.common.model.network
+
+import com.google.gson.annotations.SerializedName
+import com.makentoshe.habrachan.common.model.network.flows.GetFlowsResult
+
+sealed class Result<T>(val success: T?, val error: ErrorResult?) {
+    class GetFlowsResult2(success: GetFlowsResult?, error: ErrorResult?): Result<GetFlowsResult>(success, error)
+}
+
+data class ErrorResult(
+    @SerializedName("code")
+    val code: Int,
+    @SerializedName("message")
+    val message: String,
+    @SerializedName("additional")
+    val additional: List<String> = emptyList()
+)
