9a079fcef900143e3410fa681ebeeb377e3624f4
Makentoshe <mkliverout@gmail.com>
2019-09-28 11:07:52 +0300

    [refactor] Update Counters type

---
+++ app/src/main/java/com/makentoshe/habrachan/common/model/entity/Counters.kt
@@ -0,0 +1,17 @@
+package com.makentoshe.habrachan.common.model.entity
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Counters(
+    @SerializedName("comments")
+    val comments: Int,
+    @SerializedName("favorites")
+    val favorites: Int,
+    @SerializedName("followed")
+    val followed: Int,
+    @SerializedName("followers")
+    val followers: Int,
+    @SerializedName("posts")
+    val posts: Int
+)
