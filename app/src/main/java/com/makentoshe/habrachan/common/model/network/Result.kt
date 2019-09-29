6be4e83b6b1d255dbf3f01428f987732a3831dcd
Makentoshe <mkliverout@gmail.com>
2019-09-29 15:02:11 +0300

    [feature] Add VotePostResponse

--- app/src/main/java/com/makentoshe/habrachan/common/model/network/Result.kt
+++ app/src/main/java/com/makentoshe/habrachan/common/model/network/Result.kt
@@ -2,9 +2,11 @@ package com.makentoshe.habrachan.common.model.network
 
 import com.google.gson.annotations.SerializedName
 import com.makentoshe.habrachan.common.model.network.flows.GetFlowsResult
+import com.makentoshe.habrachan.common.model.network.votepost.VotePostResult
 
 sealed class Result<T>(val success: T?, val error: ErrorResult?) {
     class GetFlowsResponse(success: GetFlowsResult?, error: ErrorResult?): Result<GetFlowsResult>(success, error)
+    class VotePostResponse(success: VotePostResult?, error: ErrorResult?): Result<VotePostResult>(success, error)
 }
 
 data class ErrorResult(
