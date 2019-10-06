c43e05c97fa996bc6455529f1573fe1f4f0507db
Makentoshe <mkliverout@gmail.com>
2019-10-06 22:59:49 +0300

    [fix] Fix json parsing for post voting

--- app/src/main/java/com/makentoshe/habrachan/common/model/network/votepost/VotePostConverterFactory.kt
+++ app/src/main/java/com/makentoshe/habrachan/common/model/network/votepost/VotePostConverterFactory.kt
@@ -1,7 +1,5 @@
 package com.makentoshe.habrachan.common.model.network.votepost
 
-import com.google.gson.Gson
-import com.makentoshe.habrachan.common.model.network.ErrorResult
 import com.makentoshe.habrachan.common.model.network.Result
 import okhttp3.ResponseBody
 import retrofit2.Converter
@@ -10,18 +8,7 @@ import java.lang.reflect.Type
 
 class VotePostConverterFactory : Converter.Factory() {
 
-    val converter by lazy {
-        Converter<ResponseBody, Result.VotePostResponse> {
-            val json = it.string()
-            val success = Gson().fromJson(json, VotePostResult::class.java)
-            if (success.success) {
-                return@Converter Result.VotePostResponse(success, null)
-            } else {
-                val error = Gson().fromJson(json, ErrorResult::class.java)
-                return@Converter Result.VotePostResponse(null, error)
-            }
-        }
-    }
+    val converter = VotePostConverter()
 
     override fun responseBodyConverter(
         type: Type, annotations: Array<Annotation>, retrofit: Retrofit
@@ -29,3 +16,4 @@ class VotePostConverterFactory : Converter.Factory() {
         return if (type == Result.VotePostResponse::class.java) converter else null
     }
 }
+
--- app/src/test/java/com/makentoshe/habrachan/common/model/network/votepost/VotePostConverterFactoryTest.kt
+++ app/src/test/java/com/makentoshe/habrachan/common/model/network/votepost/VotePostConverterFactoryTest.kt
@@ -15,52 +15,15 @@ import java.io.File
 class VotePostConverterFactoryTest {
 
     private val factory = VotePostConverterFactory()
-    private lateinit var converter: Converter<ResponseBody, Result.VotePostResponse>
 
-    @Before
-    fun init() {
-        converter = factory.responseBodyConverter(Result.VotePostResponse::class.java, arrayOf(), mockk())!!
+    @Test
+    fun `converter should be null for incompatible type`() {
+        assertNull(factory.responseBodyConverter(Any::class.java, arrayOf(), mockk()))
     }
 
     @Test
-    fun `should parse success result`() {
-        val successJson = File(testResourcesDirectory, "VotePostSuccess.json")
-
-        val responseBody = mockk<ResponseBody>()
-        every { responseBody.string() } returns successJson.readText()
-
-        converter.convert(responseBody).also {
-            assertNull(it.error)
-            assertNotNull(it.success)
-            assertEquals(1, it.success!!.score)
-            assertEquals("2019-09-29T14:21:30+03:00", it.success!!.serverTime)
-            assertTrue(it.success!!.success)
-        }
+    fun `converter should not be null`() {
+        assertNotNull(factory.responseBodyConverter(Result.VotePostResponse::class.java, arrayOf(), mockk()))
     }
 
-    @Test
-    fun `should parse api error result`() {
-        val errorJson = File(testResourcesDirectory, "ErrorResult.json")
-
-        val responseBody = mockk<ResponseBody>()
-        every { responseBody.string() } returns errorJson.readText()
-
-        converter.convert(responseBody).also {
-            assertNotNull(it.error)
-            assertNull(it.success)
-        }
-    }
-
-    @Test
-    fun `should parse error result`() {
-        val errorJson = File(testResourcesDirectory, "VotePostError.json")
-
-        val responseBody = mockk<ResponseBody>()
-        every { responseBody.string() } returns errorJson.readText()
-
-        converter.convert(responseBody).also {
-            assertNotNull(it.error)
-            assertNull(it.success)
-        }
-    }
 }
