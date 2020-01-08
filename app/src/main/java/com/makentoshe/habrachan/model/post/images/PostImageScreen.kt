1f412ee0d2824875541c021f971400f8100b4b3f
Makentoshe <mkliverout@gmail.com>
2020-01-08 03:02:50 +0300

    [feature] Update 'fragment' val type from androidx.Fragment to PostImageFragmentPage

--- app/src/main/java/com/makentoshe/habrachan/model/post/images/PostImageScreen.kt
+++ app/src/main/java/com/makentoshe/habrachan/model/post/images/PostImageScreen.kt
@@ -1,14 +1,12 @@
 package com.makentoshe.habrachan.model.post.images
 
-import androidx.fragment.app.Fragment
 import com.makentoshe.habrachan.common.navigation.Screen
 import com.makentoshe.habrachan.view.post.images.PostImageFragmentPage
-import com.makentoshe.habrachan.view.post.images.PostImagesFragment
 
-class PostImagesScreen(
+class PostImageScreen(
     private val index: Int,
     private val sources: Array<String>
 ) : Screen() {
-    override val fragment: Fragment
+    override val fragment: PostImageFragmentPage
         get() = PostImageFragmentPage.Factory().build(index, sources[index])
 }
