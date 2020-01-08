5916ed25fedbde4be0216add3b17955364c495ef
Makentoshe <mkliverout@gmail.com>
2020-01-08 03:03:40 +0300

    [feature] Add ui class for PostImageFragmentPage

--- app/src/main/java/com/makentoshe/habrachan/ui/post/images/PostImageFragmentPageUi.kt
+++ app/src/main/java/com/makentoshe/habrachan/ui/post/images/PostImageFragmentPageUi.kt
@@ -1,4 +1,12 @@
 package com.makentoshe.habrachan.ui.post.images
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import com.makentoshe.habrachan.R
+
 class PostImageFragmentPageUi {
+    fun createView(context: Context): View {
+        return LayoutInflater.from(context).inflate(R.layout.post_image_fragment, null, false)
+    }
 }
