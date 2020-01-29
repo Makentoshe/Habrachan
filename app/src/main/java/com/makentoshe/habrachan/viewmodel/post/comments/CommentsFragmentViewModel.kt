8e2218dbe7af02a85eaeeec73f6574a3fe8c6fd5
Makentoshe <mkliverout@gmail.com>
2020-01-29 18:30:04 +0300

    app/src/main/java/com/makentoshe/habrachan/di/post/comments/CommentsFragmentModule.kt

--- app/src/main/java/com/makentoshe/habrachan/viewmodel/post/comments/CommentsFragmentViewModel.kt
+++ app/src/main/java/com/makentoshe/habrachan/viewmodel/post/comments/CommentsFragmentViewModel.kt
@@ -2,17 +2,64 @@ package com.makentoshe.habrachan.viewmodel.post.comments
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.makentoshe.habrachan.common.entity.comment.CommentsResponse
+import com.makentoshe.habrachan.common.network.manager.HabrCommentsManager
+import com.makentoshe.habrachan.common.network.request.GetCommentsRequest
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subjects.BehaviorSubject
 
-class CommentsFragmentViewModel(private val articleId: Int) : ViewModel() {
+class CommentsFragmentViewModel(
+    private val articleId: Int,
+    private val commentsManager: HabrCommentsManager,
+    private val commentsRequestFactory: GetCommentsRequest.Factory
+) : ViewModel() {
+
+    private val disposables = CompositeDisposable()
+
+    private val successSubject = BehaviorSubject.create<CommentsResponse>()
+    val successObservable: Observable<CommentsResponse>
+        get() = successSubject.observeOn(AndroidSchedulers.mainThread())
+
+    private val errorSubject = BehaviorSubject.create<Throwable>()
+    val errorObservable: Observable<Throwable>
+        get() = errorSubject.observeOn(AndroidSchedulers.mainThread())
+
+    private val progressSubject = BehaviorSubject.create<Unit>()
+    val progressObservable: Observable<Unit>
+        get() = progressSubject.observeOn(AndroidSchedulers.mainThread())
 
     init {
-        // start load using article id
-        println(articleId)
+        requestComments()
     }
 
-    class Factory(private val articleId: Int) : ViewModelProvider.NewInstanceFactory() {
+    fun requestComments() {
+        val request = commentsRequestFactory.build(articleId)
+        commentsManager.getComments(request).subscribe(::onSuccess, ::onError).let(disposables::add)
+    }
+
+    private fun onSuccess(response: CommentsResponse) {
+        successSubject.onNext(response)
+        errorSubject.onComplete()
+        progressSubject.onComplete()
+    }
+
+    private fun onError(throwable: Throwable) {
+        errorSubject.onNext(throwable)
+    }
+
+    override fun onCleared() {
+        disposables.clear()
+    }
+
+    class Factory(
+        private val articleId: Int,
+        private val commentsManager: HabrCommentsManager,
+        private val commentsRequestFactory: GetCommentsRequest.Factory
+    ) : ViewModelProvider.NewInstanceFactory() {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            return CommentsFragmentViewModel(articleId) as T
+            return CommentsFragmentViewModel(articleId, commentsManager, commentsRequestFactory) as T
         }
     }
 }
