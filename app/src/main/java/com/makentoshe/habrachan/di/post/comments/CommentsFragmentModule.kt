f1d14dcb31dc56c7fc391c0c5a642b8c65c6c0b2
Makentoshe <mkliverout@gmail.com>
2020-01-29 18:30:50 +0300

    [feature] Add comments downloading

--- app/src/main/java/com/makentoshe/habrachan/di/post/comments/CommentsFragmentModule.kt
+++ app/src/main/java/com/makentoshe/habrachan/di/post/comments/CommentsFragmentModule.kt
@@ -1,10 +1,15 @@
 package com.makentoshe.habrachan.di.post.comments
 
 import androidx.lifecycle.ViewModelProviders
+import com.makentoshe.habrachan.common.network.manager.HabrCommentsManager
+import com.makentoshe.habrachan.common.network.request.GetCommentsRequest
+import com.makentoshe.habrachan.di.ApplicationScope
 import com.makentoshe.habrachan.view.post.comments.CommentsFragment
 import com.makentoshe.habrachan.viewmodel.post.comments.CommentsFragmentViewModel
+import toothpick.Toothpick
 import toothpick.config.Module
 import toothpick.ktp.binding.bind
+import toothpick.ktp.delegate.inject
 import javax.inject.Provider
 
 annotation class CommentsFragmentScope
@@ -26,8 +31,17 @@ class CommentsFragmentModule(fragment: CommentsFragment) : Module() {
         private val fragment: CommentsFragment
     ) : Provider<CommentsFragmentViewModel> {
 
+        private val commentsManager by inject<HabrCommentsManager>()
+        private val getCommentsRequestFactory by inject<GetCommentsRequest.Factory>()
+
+        init {
+            Toothpick.openScope(ApplicationScope::class.java).inject(this)
+        }
+
         override fun get(): CommentsFragmentViewModel {
-            val factory = CommentsFragmentViewModel.Factory(fragment.arguments.articleId)
+            val factory = CommentsFragmentViewModel.Factory (
+                fragment.arguments.articleId, commentsManager, getCommentsRequestFactory
+            )
             return ViewModelProviders.of(fragment, factory)[CommentsFragmentViewModel::class.java]
         }
     }
