972bbcaf1b06702db33c0ec584f9a16094cf0c42
Makentoshe <mkliverout@gmail.com>
2020-02-20 18:34:13 +0300

    [feature] Add ui for user screen

--- app/src/main/java/com/makentoshe/habrachan/ui/main/account/user/UserFragmentUi.kt
+++ app/src/main/java/com/makentoshe/habrachan/ui/main/account/user/UserFragmentUi.kt
@@ -1,14 +1,13 @@
 package com.makentoshe.habrachan.ui.main.account.user
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
+import com.makentoshe.habrachan.R
 
 class UserFragmentUi {
     fun create(context: Context): View {
-        return TextView(context).apply {
-            text = "USER FRAGMENT"
-        }
-//        return LayoutInflater.from(context).inflate()
+        return LayoutInflater.from(context).inflate(R.layout.user_fragment, null, false)
     }
 }
--- app/src/main/java/com/makentoshe/habrachan/view/main/account/user/UserFragment.kt
+++ app/src/main/java/com/makentoshe/habrachan/view/main/account/user/UserFragment.kt
@@ -1,11 +1,22 @@
 package com.makentoshe.habrachan.view.main.account.user
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.TimeInterpolator
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.MarginLayoutParams
+import android.view.ViewPropertyAnimator
 import android.widget.Toast
+import androidx.annotation.Dimension
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.animation.AnimationUtils
 import com.makentoshe.habrachan.ui.main.account.user.UserFragmentUi
 import com.makentoshe.habrachan.viewmodel.main.account.user.UserViewModel
 import io.reactivex.disposables.CompositeDisposable
