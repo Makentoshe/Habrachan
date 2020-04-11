7286e3d5a5509537cf46f5e5226b141c3a0cac17
Makentoshe <mkliverout@gmail.com>
2020-04-11 16:00:41 +0300

    feat(articles): Add new articles requests

--- app/src/main/java/com/makentoshe/habrachan/common/network/request/GetArticlesRequest.kt
+++ app/src/main/java/com/makentoshe/habrachan/common/network/request/GetArticlesRequest.kt
@@ -1,14 +1,27 @@
 package com.makentoshe.habrachan.common.network.request
 
+import com.makentoshe.habrachan.common.entity.session.UserSession
+
 data class GetArticlesRequest(
     val client: String,
     val api: String,
     val token: String,
     val page: Int,
     val spec: String
-)  {
+) {
 
-    class Factory(private val client: String, private val api: String, private val token: String) {
-        fun all(page: Int) = GetArticlesRequest(client, api, token, page, "posts/all")
+    class Factory(private val session: UserSession) {
+
+        fun all(page: Int): GetArticlesRequest {
+            return GetArticlesRequest(session.clientKey, session.apiKey, session.tokenKey, page, "posts/all")
+        }
+
+        fun interesting(page: Int): GetArticlesRequest {
+            return GetArticlesRequest(session.clientKey, session.apiKey, session.tokenKey, page, "posts/interesting")
+        }
+
+        fun custom(page: Int, spec: String): GetArticlesRequest {
+            return GetArticlesRequest(session.clientKey, session.apiKey, session.tokenKey, page, spec)
+        }
     }
 }
