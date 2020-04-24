8410e50e3963cf120f8bd1b7eed61b081f499677
Makentoshe <mkliverout@gmail.com>
2020-04-24 21:52:08 +0300

    feat(user): Add di module for user articles

---
+++ app/src/main/java/com/makentoshe/habrachan/di/user/UserArticlesFragmentModule.kt
@@ -0,0 +1,16 @@
+package com.makentoshe.habrachan.di.user
+
+import com.makentoshe.habrachan.view.user.UserArticlesFragment
+import toothpick.config.Module
+
+class UserArticlesFragmentModule(fragment: UserArticlesFragment) : Module() {
+
+    init {
+
+    }
+
+//    private fun getArticlesViewModel(fragment: ArticlesFragment): ArticlesViewModel {
+//        val factory = ArticlesViewModel.Factory(articlesManager, database, sessionDatabase, router)
+//        return ViewModelProviders.of(fragment, factory)[ArticlesViewModel::class.java]
+//    }
+}
