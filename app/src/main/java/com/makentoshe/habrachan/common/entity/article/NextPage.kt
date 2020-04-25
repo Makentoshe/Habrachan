26dab8eac54977aafe67a5fa14ec875f79ea1f5b
Makentoshe <mkliverout@gmail.com>
2020-04-26 01:04:02 +0300

    feat(user): Add caching for user articles

--- app/src/main/java/com/makentoshe/habrachan/common/entity/article/NextPage.kt
+++ app/src/main/java/com/makentoshe/habrachan/common/entity/article/NextPage.kt
@@ -7,9 +7,9 @@ data class NextPage(
     @SerializedName("int")
     val int: Int,
     @SerializedName("url")
-    val url: String
+    val url: String?
 ) {
-    @Suppress("SENSELESS_COMPARISON")
+
     val isSpecified: Boolean
         get() = url != null
 }
