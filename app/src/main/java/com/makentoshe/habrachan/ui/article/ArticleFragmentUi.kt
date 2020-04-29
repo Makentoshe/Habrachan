6f160d5eafc8dae61941f2c9f09b09015e4e44bd
Makentoshe <mkliverout@gmail.com>
2020-04-29 18:12:36 +0300

    refactor(article): Rename PostFragmentUi to ArticleFragmentUi

--- app/src/main/java/com/makentoshe/habrachan/ui/article/ArticleFragmentUi.kt
+++ app/src/main/java/com/makentoshe/habrachan/ui/article/ArticleFragmentUi.kt
@@ -3,10 +3,11 @@ package com.makentoshe.habrachan.ui.article
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import com.makentoshe.habrachan.R
 
-class PostFragmentUi {
+class ArticleFragmentUi(private val root: ViewGroup?) {
     fun createView(context: Context): View {
-        return LayoutInflater.from(context).inflate(R.layout.article_fragment, null, false)
+        return LayoutInflater.from(context).inflate(R.layout.article_fragment, root, false)
     }
 }
