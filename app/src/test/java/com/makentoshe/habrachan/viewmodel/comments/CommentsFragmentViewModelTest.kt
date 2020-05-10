d3db586f5bc03bda103f2ed1de73023cfe1daa28
Makentoshe <mkliverout@gmail.com>
2020-05-10 18:12:19 +0300

    test(comments): Add tests for CommentsFragmentViewModel
    Tests related for VoteCommentViewModel interface

--- app/src/test/java/com/makentoshe/habrachan/viewmodel/comments/CommentsFragmentViewModelTest.kt
+++ app/src/test/java/com/makentoshe/habrachan/viewmodel/comments/CommentsFragmentViewModelTest.kt
@@ -1,15 +1,19 @@
 package com.makentoshe.habrachan.viewmodel.comments
 
+import com.google.android.material.appbar.AppBarLayout
 import com.makentoshe.habrachan.common.database.CacheDatabase
 import com.makentoshe.habrachan.common.database.session.SessionDatabase
+import com.makentoshe.habrachan.common.entity.comment.Comment
 import com.makentoshe.habrachan.common.network.manager.HabrCommentsManager
 import com.makentoshe.habrachan.common.network.manager.ImageManager
 import com.makentoshe.habrachan.common.network.response.GetCommentsResponse
+import com.makentoshe.habrachan.common.network.response.VoteCommentResponse
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.BehaviorSubject
 import org.junit.Before
 import org.junit.Test
 
@@ -34,7 +38,7 @@ class CommentsFragmentViewModelTest {
     }
 
     @Test
-    fun testShouldReturnResponseOnRequest() {
+    fun testShouldReturnGetCommentsResponseOnRequest() {
         val mockResponse = mockk<GetCommentsResponse>(relaxed = true)
         every { mockCommentsManager.getComments(any()) } returns Single.just(mockResponse)
 
@@ -77,4 +81,105 @@ class CommentsFragmentViewModelTest {
         verify { mockCacheDatabase.comments().getByArticleId(articleId) }
     }
 
+    @Test
+    fun testShouldReplaceCommentInDatabaseOnSuccessUpVote() {
+        val mockComment = mockk<Comment>(relaxed = true)
+        every { mockCacheDatabase.comments().getById(any()) } returns mockComment
+
+        val mockResponse = mockk<VoteCommentResponse.Success>(relaxed = true)
+        every { mockResponse.score } returns 10
+        every { mockCommentsManager.voteUp(any()) } returns Single.just(mockResponse)
+
+        val commentId = 39
+        viewModel.voteUpCommentObserver.onNext(commentId)
+
+        verify { mockCacheDatabase.comments().insert(any()) }
+        verify { mockComment.copy(score = 10) }
+    }
+
+    @Test
+    fun testShouldReturnErrorVoteUpCommentResponseWithoutCaching() {
+        val mockComment = mockk<Comment>(relaxed = true)
+        every { mockCacheDatabase.comments().getById(any()) } returns mockComment
+
+        val mockResponse = mockk<VoteCommentResponse.Error>(relaxed = true)
+        every { mockCommentsManager.voteUp(any()) } returns Single.just(mockResponse)
+
+        val commentId = 39
+        viewModel.voteUpCommentObserver.onNext(commentId)
+
+        verify(exactly = 0) { mockCacheDatabase.comments().insert(any()) }
+    }
+
+    @Test
+    fun testShouldResponseOnVoteUpRequest() {
+        val testSubject = viewModel.voteUpCommentObservable.test()
+
+        val mockComment = mockk<Comment>(relaxed = true)
+        every { mockCacheDatabase.comments().getById(any()) } returns mockComment
+
+        val mockResponse = mockk<VoteCommentResponse.Success>(relaxed = true)
+        every { mockCommentsManager.voteUp(any()) } returns Single.just(mockResponse)
+
+        val commentId = 39
+        viewModel.voteUpCommentObserver.onNext(commentId)
+
+        testSubject.assertValues(mockResponse).dispose()
+    }
+
+    @Test
+    fun testShouldResponseOnVoteDownRequest() {
+        val testSubject = viewModel.voteDownCommentObservable.test()
+
+        val mockComment = mockk<Comment>(relaxed = true)
+        every { mockCacheDatabase.comments().getById(any()) } returns mockComment
+
+        val mockResponse = mockk<VoteCommentResponse.Error>(relaxed = true)
+        every { mockCommentsManager.voteDown(any()) } returns Single.just(mockResponse)
+
+        val commentId = 39
+        viewModel.voteDownCommentObserver.onNext(commentId)
+
+        testSubject.assertValues(mockResponse).dispose()
+    }
+
+    @Test
+    fun testShouldReturnErrorResponseOnVoteUpException() {
+        val testObservable = viewModel.voteUpCommentObservable.test()
+
+        every { mockCommentsManager.voteUp(any()) } returns Single.just(Exception()).map { throw it }
+
+        val commentId = 39
+        viewModel.voteUpCommentObserver.onNext(commentId)
+
+        testObservable.assertValue { it is VoteCommentResponse.Error }.dispose()
+    }
+
+    @Test
+    fun testShouldReplaceCommentInDatabaseOnSuccessDownVote() {
+        val mockComment = mockk<Comment>(relaxed = true)
+        every { mockCacheDatabase.comments().getById(any()) } returns mockComment
+
+        val mockResponse = mockk<VoteCommentResponse.Success>(relaxed = true)
+        every { mockResponse.score } returns 10
+        every { mockCommentsManager.voteDown(any()) } returns Single.just(mockResponse)
+
+        val commentId = 39
+        viewModel.voteDownCommentObserver.onNext(commentId)
+
+        verify { mockCacheDatabase.comments().insert(any()) }
+        verify { mockComment.copy(score = 10) }
+    }
+
+    @Test
+    fun sas() {
+        val testObservable = viewModel.voteDownCommentObservable.test()
+
+        every { mockCommentsManager.voteDown(any()) } returns Single.just(Exception()).map { throw it }
+
+        val commentId = 39
+        viewModel.voteDownCommentObserver.onNext(commentId)
+
+        testObservable.assertValue { it is VoteCommentResponse.Error }.dispose()
+    }
 }
