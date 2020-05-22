e9092eb9c43cb8837d24e0ec103f8919a616f251
Makentoshe <mkliverout@gmail.com>
2020-05-22 19:24:27 +0300

    test(navigation): Add tests for UserNavigator class.

--- app/src/main/java/com/makentoshe/habrachan/navigation/user/UserNavigator.kt
+++ app/src/main/java/com/makentoshe/habrachan/navigation/user/UserNavigator.kt
@@ -1,6 +1,6 @@
 package com.makentoshe.habrachan.navigation.user
 
-import com.makentoshe.habrachan.navigation.Router
+import ru.terrakok.cicerone.Router
 
 class UserNavigator(private val router: Router) {
     fun back() = router.exit()
