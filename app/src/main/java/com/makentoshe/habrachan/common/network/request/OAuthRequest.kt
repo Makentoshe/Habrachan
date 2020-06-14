4ead6568e9093be5b920d4c86046925b4a473052
Makentoshe <mkliverout@gmail.com>
2020-06-14 15:08:29 +0300

    refactor(login): Increase code flexibility by passing query params from the request object

--- app/src/main/java/com/makentoshe/habrachan/common/network/request/OAuthRequest.kt
+++ app/src/main/java/com/makentoshe/habrachan/common/network/request/OAuthRequest.kt
@@ -1,3 +1,9 @@
 package com.makentoshe.habrachan.common.network.request
 
-data class OAuthRequest(val socialType: String)
+data class OAuthRequest(
+    val clientId: String,
+    val socialType: String,
+    val hostUrl: String,
+    val responseType: String = "token",
+    val redirectUri: String = "http://cleverpumpkin.ru"
+)
