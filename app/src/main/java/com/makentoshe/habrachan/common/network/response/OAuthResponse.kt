fb677377164ae941ef059d4dc963fc7e48e856fb
Makentoshe <mkliverout@gmail.com>
2020-06-14 14:59:39 +0300

    feat(login): Add OAuthResponse.Error data class for handling oauth errors

--- app/src/main/java/com/makentoshe/habrachan/common/network/response/OAuthResponse.kt
+++ app/src/main/java/com/makentoshe/habrachan/common/network/response/OAuthResponse.kt
@@ -1,8 +1,11 @@
 package com.makentoshe.habrachan.common.network.response
 
 sealed class OAuthResponse {
+
     data class Success(
         val redirectUrl: String,
         val state: String
     ) : OAuthResponse()
+
+    data class Error(val string: String): OAuthResponse()
 }
