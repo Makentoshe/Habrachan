0c3b455e2f5320f77684968847212514b3aefd41
Makentoshe <mkliverout@gmail.com>
2020-12-08 19:27:14 +0300

    fix(android): Fix logging

--- application/android/core/src/main/java/com/makentoshe/habrachan/application/android/arena/ArticlesArenaCache.kt
+++ application/android/core/src/main/java/com/makentoshe/habrachan/application/android/arena/ArticlesArenaCache.kt
@@ -1,6 +1,7 @@
 package com.makentoshe.habrachan.application.android.arena
 
 import android.util.Log
+import com.makentoshe.habrachan.application.android.BuildConfig
 import com.makentoshe.habrachan.application.android.database.AndroidCacheDatabase
 import com.makentoshe.habrachan.application.android.database.record.ArticleRecord
 import com.makentoshe.habrachan.application.core.arena.ArenaCache
@@ -14,21 +15,21 @@ class ArticlesArenaCache(
 ) : ArenaCache<GetArticlesRequest, ArticlesResponse> {
 
     companion object {
-        fun capture(priority: Int, string: String) {
-            Log.println(priority, "ArticlesArenaCache", string)
+        fun debug(priority: Int, string: String) {
+            if (BuildConfig.DEBUG) Log.println(priority, "ArticlesArenaCache", string)
         }
     }
 
     override fun fetch(key: GetArticlesRequest): Result<ArticlesResponse> {
-        capture(Log.DEBUG, "Fetch search articles from cache by key: $key")
+        debug(Log.DEBUG, "Fetch search articles from cache by key: $key")
         return try {
             val start = (key.page - 1) * key.count
             val end = start + key.count - 1
             val articles = cacheDatabase.articlesSearchDao().getInRange(start, end).map { it.toArticle() }
-            capture(Log.INFO, "Fetched ${articles.size} articles from $start to $end")
+            debug(Log.INFO, "Fetched ${articles.size} articles from $start to $end")
             Result.success(ArticlesResponse(articles, NextPage(key.page + 1, ""), -1, "", "", null))
         } catch (exception: Exception) {
-            capture(Log.INFO, "Could not fetch articles: ${exception.toString()}")
+            debug(Log.INFO, "Could not fetch articles: ${exception.toString()}")
             Result.failure(ArenaStorageException("ArticlesArenaCache").initCause(exception))
         }
     }
@@ -37,14 +38,14 @@ class ArticlesArenaCache(
     override fun carry(key: GetArticlesRequest, value: ArticlesResponse) {
         // clear cache on new search (each search starts from page 1)
         if (key.page == 1) {
-            capture(Log.INFO, "Clear search articles cache")
+            debug(Log.INFO, "Clear search articles cache")
             cacheDatabase.articlesSearchDao().clear()
         }
 
-        capture(Log.DEBUG, "Carry search articles to cache with key: $key")
+        debug(Log.DEBUG, "Carry search articles to cache with key: $key")
         value.data.forEachIndexed { index, article ->
             val databaseIndex = (key.page - 1) * key.count + index
-            capture(Log.INFO, "Carry $databaseIndex article to cache")
+            debug(Log.INFO, "Carry $databaseIndex article to cache")
             cacheDatabase.articlesSearchDao().insert(ArticleRecord(databaseIndex, article))
         }
     }
