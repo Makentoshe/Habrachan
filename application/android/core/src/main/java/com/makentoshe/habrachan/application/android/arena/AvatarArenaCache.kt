d457d37080657772b3d60c524bd67ad37dc9e719
Makentoshe <mkliverout@gmail.com>
2020-12-10 17:47:55 +0300

    feat(application): Add AvatarArenaCache
    Cache for AvatarArena

---
+++ application/android/core/src/main/java/com/makentoshe/habrachan/application/android/arena/AvatarArenaCache.kt
@@ -0,0 +1,17 @@
+package com.makentoshe.habrachan.application.android.arena
+
+import com.makentoshe.habrachan.application.core.arena.ArenaCache
+import com.makentoshe.habrachan.application.core.arena.ArenaStorageException
+import com.makentoshe.habrachan.network.request.ImageRequest
+import com.makentoshe.habrachan.network.response.ImageResponse
+
+class AvatarArenaCache: ArenaCache<ImageRequest, ImageResponse> {
+
+    override fun fetch(key: ImageRequest): Result<ImageResponse> {
+        return Result.failure(ArenaStorageException("AvatarArenaCache"))
+    }
+
+    override fun carry(key: ImageRequest, value: ImageResponse) {
+
+    }
+}
