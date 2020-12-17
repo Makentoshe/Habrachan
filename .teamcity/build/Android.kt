1c30483c16f205ec854b503da78803b7d24f8946
Makentoshe <mkliverout@gmail.com>
2020-12-17 21:47:50 +0300

    ci(android): Refactor TeamCity builds - Add updating android sdk 29

--- .teamcity/build/Android.kt
+++ .teamcity/build/Android.kt
@@ -5,7 +5,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.BuildStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import reference
 
-object Android: PipelineBuildVcs("Android", {
+object Android : PipelineBuildVcs("Android", {
     steps {
         script {
             name = "Clean Android home directory"
@@ -16,17 +16,26 @@ object Android: PipelineBuildVcs("Android", {
             name = "Install Android SDK tools"
             executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
             scriptContent = """
-                mkdir "%env.ANDROID_HOME%"
-                cd "%env.ANDROID_HOME%"
-                curl -o sdk.zip "%ANDROID_SDK_URL%"
+                mkdir "${Parameters.AndroidHome.reference}"
+                cd "${Parameters.AndroidHome.reference}"
+                curl -o sdk.zip "${Parameters.AndroidSdkUrl.reference}"
                 unzip sdk.zip
             """.trimIndent()
         }
         script {
             name = "Accept licences for Android SDK"
             scriptContent = """
-                mkdir -p "%env.ANDROID_HOME%/licenses"
-                echo "24333f8a63b6825ea9c5514f83c2829b004d1fee" > "%env.ANDROID_HOME%/licenses/android-sdk-license"
+                mkdir -p "${Parameters.AndroidHome.reference}/licenses"
+                echo "24333f8a63b6825ea9c5514f83c2829b004d1fee" > "${Parameters.AndroidHome.reference}/licenses/android-sdk-license"
+            """.trimIndent()
+        }
+        script {
+            val androidVersion = "29"
+            val androidBuildToolsVersion = "29.0.3"
+            name = "Update Android SDK 29"
+            scriptContent = """
+                ${Parameters.AndroidHome.reference}/tools/bin/sdkmanager --update
+                ${Parameters.AndroidHome.reference}/tools/bin/sdkmanager "build-tools;$androidBuildToolsVersion" "platforms;android-$androidVersion" "platform-tools"
             """.trimIndent()
         }
     }
