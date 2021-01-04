acf91e94fb9790ec6c6eff0fb8c14d286724404e
Makentoshe <mkliverout@gmail.com>
2021-01-04 19:23:04 +0300

    feat(android): Implement empty comments arena cache

--- application/android/core/src/main/java/com/makentoshe/habrachan/application/android/arena/CommentsArenaCache.kt
+++ application/android/core/src/main/java/com/makentoshe/habrachan/application/android/arena/CommentsArenaCache.kt
@@ -1,5 +1,7 @@
 package com.makentoshe.habrachan.application.android.arena
 
+import android.util.Log
+import com.makentoshe.habrachan.application.android.BuildConfig
 import com.makentoshe.habrachan.application.core.arena.ArenaCache
 import com.makentoshe.habrachan.application.core.arena.ArenaStorageException
 import com.makentoshe.habrachan.entity.Comment
@@ -7,11 +9,19 @@ import com.makentoshe.habrachan.network.request.GetCommentsRequest
 
 class CommentsArenaCache: ArenaCache<GetCommentsRequest, List<Comment>> {
 
+    companion object {
+        fun capture(level: Int, message: () -> String) {
+            if (!BuildConfig.DEBUG) return
+            Log.println(level, "CommentsArenaCache", message())
+        }
+    }
+
     override fun fetch(key: GetCommentsRequest): Result<List<Comment>> {
+        capture(Log.INFO) { "fetch not implemented" }
         return Result.failure(ArenaStorageException("fetch not implemented"))
     }
 
     override fun carry(key: GetCommentsRequest, value: List<Comment>) {
-
+        capture(Log.INFO) { "carry not implemented" }
     }
 }
