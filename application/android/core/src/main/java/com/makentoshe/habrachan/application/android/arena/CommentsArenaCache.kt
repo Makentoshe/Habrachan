3f2bd0bde7b7758754df9a48747a1768032a07be
Makentoshe <mkliverout@gmail.com>
2021-01-04 21:10:51 +0300

    feat(android): Implement CommentsArenaCache

--- application/android/core/src/main/java/com/makentoshe/habrachan/application/android/arena/CommentsArenaCache.kt
+++ application/android/core/src/main/java/com/makentoshe/habrachan/application/android/arena/CommentsArenaCache.kt
@@ -3,6 +3,7 @@ package com.makentoshe.habrachan.application.android.arena
 import android.util.Log
 import com.makentoshe.habrachan.application.android.BuildConfig
 import com.makentoshe.habrachan.application.android.database.dao.CommentDao
+import com.makentoshe.habrachan.application.android.database.record.CommentRecord
 import com.makentoshe.habrachan.application.core.arena.ArenaCache
 import com.makentoshe.habrachan.application.core.arena.ArenaStorageException
 import com.makentoshe.habrachan.entity.Comment
@@ -10,23 +11,37 @@ import com.makentoshe.habrachan.network.request.GetCommentsRequest
 
 class CommentsArenaCache(
     private val commentDao: CommentDao
-): ArenaCache<GetCommentsRequest, List<Comment>> {
+) : ArenaCache<GetCommentsRequest, List<Comment>> {
 
     companion object {
+        private const val limit = 3000
+        private const val step = 500
+
         fun capture(level: Int, message: () -> String) {
             if (!BuildConfig.DEBUG) return
             Log.println(level, "CommentsArenaCache", message())
         }
     }
 
-    override fun fetch(key: GetCommentsRequest): Result<List<Comment>> {
-        capture(Log.INFO) { "fetch not implemented" }
-        // TODO not implemented
-        return Result.failure(ArenaStorageException("fetch not implemented"))
+    override fun fetch(key: GetCommentsRequest): Result<List<Comment>> = try {
+        val records = commentDao.getByArticleId(key.articleId)
+        capture(Log.INFO) { "Fetched ${records.size} comments by key: $key" }
+        Result.success(records.map { it.toComment() })
+    } catch (exception: Exception) {
+        capture(Log.INFO) { "Could not fetch comments: $exception"}
+        Result.failure(ArenaStorageException("CommentsArenaCache").initCause(exception))
     }
 
     override fun carry(key: GetCommentsRequest, value: List<Comment>) {
-        capture(Log.INFO) { "carry not implemented" }
-        // TODO not implemented
+        if (commentDao.count() > limit) capture(Log.WARN) {
+            // TODO implement removing oldest elements
+            "TODO: Removing oldest $step elements from cache"
+        }
+
+        // TODO if the element already placed in the database - reset it lifecycle index
+        capture(Log.INFO) {"Carry ${value.size} comments to cache with key: $key" }
+        value.map { comment -> CommentRecord(key.articleId, comment) }.forEach { record ->
+            commentDao.insert(record)
+        }
     }
 }
