eb5b13f69e1ba0c3fe53dc30a5b1d9c7113e7fa8
Makentoshe <mkliverout@gmail.com>
2021-01-04 19:17:47 +0300

    feat(application): Add cache first arena for comments

--- application/core/src/main/kotlin/com/makentoshe/habrachan/application/core/arena/comments/CommentsCacheFirstArena.kt
+++ application/core/src/main/kotlin/com/makentoshe/habrachan/application/core/arena/comments/CommentsCacheFirstArena.kt
@@ -1,18 +1,17 @@
 package com.makentoshe.habrachan.application.core.arena.comments
 
 import com.makentoshe.habrachan.application.core.arena.ArenaCache
-import com.makentoshe.habrachan.application.core.arena.SourceFirstArena
+import com.makentoshe.habrachan.application.core.arena.CacheFirstArena
 import com.makentoshe.habrachan.entity.Comment
 import com.makentoshe.habrachan.network.manager.CommentsManager
 import com.makentoshe.habrachan.network.request.GetCommentsRequest
 
-class CommentsArena(
+class CommentsCacheFirstArena(
     private val commentsManager: CommentsManager,
     cache: ArenaCache<GetCommentsRequest, List<Comment>>
-) : SourceFirstArena<GetCommentsRequest, List<Comment>>(cache) {
+): CacheFirstArena<GetCommentsRequest, List<Comment>>(cache) {
 
     override suspend fun internalSuspendFetch(key: GetCommentsRequest): Result<List<Comment>> {
         return commentsManager.getComments(key).fold({ Result.success(it.data) }, { Result.failure(it) })
     }
 }
-
