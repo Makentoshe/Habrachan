81d8c0d1f4860cf932b20f8db28d6adcd0750639
Makentoshe <mkliverout@gmail.com>
2021-01-29 17:52:57 +0300

    feat(core): Add interface for ArticlesResponse

--- library/network/src/main/kotlin/com/makentoshe/habrachan/network/response/GetArticlesResponse.kt
+++ library/network/src/main/kotlin/com/makentoshe/habrachan/network/response/GetArticlesResponse.kt
@@ -5,17 +5,28 @@ import com.makentoshe.habrachan.entity.natives.Article
 import com.makentoshe.habrachan.entity.natives.NextPage
 import com.makentoshe.habrachan.entity.natives.User
 
+interface GetArticlesResponse {
+
+    val articles: List<com.makentoshe.habrachan.entity.Article>
+
+    val pages: Int
+}
+
 data class ArticlesResponse(
     @SerializedName("data")
     val data: List<Article>,
     @SerializedName("next_page")
     val nextPage: NextPage,
     @SerializedName("pages")
-    val pages: Int,
+    override val pages: Int,
     @SerializedName("server_time")
     val serverTime: String,
     @SerializedName("sorted_by")
     val sortedBy: String,
     @SerializedName("author")
     val author: User? = null
-)
+): GetArticlesResponse {
+    override val articles: List<com.makentoshe.habrachan.entity.Article>
+        get() = data
+}
+
