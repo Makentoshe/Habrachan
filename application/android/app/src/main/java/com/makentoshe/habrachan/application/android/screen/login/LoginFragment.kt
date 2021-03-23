21853fb926e82b9961a16938c3b8c2953cce3804
Makentoshe <mkliverout@gmail.com>
2021-03-23 16:56:54 +0300

    feat(android): Add di and viewmodel for the login screen

--- application/android/app/src/main/java/com/makentoshe/habrachan/application/android/screen/login/LoginFragment.kt
+++ application/android/app/src/main/java/com/makentoshe/habrachan/application/android/screen/login/LoginFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import com.makentoshe.habrachan.R
 import com.makentoshe.habrachan.application.android.CoreFragment
+import com.makentoshe.habrachan.application.android.screen.login.viewmodel.LoginViewModel
+import toothpick.ktp.delegate.inject
 
 class LoginFragment : CoreFragment() {
 
@@ -15,12 +17,14 @@ class LoginFragment : CoreFragment() {
 
     override val arguments = Arguments(this)
 
+    private val viewModel by inject<LoginViewModel>()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        view.setOnClickListener {  }
     }
 
     class Arguments(fragment: LoginFragment) : CoreFragment.Arguments(fragment)
