17798f132d58df345769d60223c85a7c404764c7
Makentoshe <mkliverout@gmail.com>
2021-03-24 22:57:39 +0300

    feat(android): Implement viewmodel and di for login screen

--- application/android/app/src/main/java/com/makentoshe/habrachan/application/android/screen/login/LoginFragment.kt
+++ application/android/app/src/main/java/com/makentoshe/habrachan/application/android/screen/login/LoginFragment.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.makentoshe.habrachan.R
 import com.makentoshe.habrachan.application.android.CoreFragment
+import com.makentoshe.habrachan.application.android.screen.login.model.LoginSpec
 import com.makentoshe.habrachan.application.android.screen.login.viewmodel.LoginViewModel
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import toothpick.ktp.delegate.inject
 
 class LoginFragment : CoreFragment() {
@@ -24,7 +29,21 @@ class LoginFragment : CoreFragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.setOnClickListener {  }
+        view.setOnClickListener { /* workaround */ }
+
+        fragment_login_button.setOnClickListener {
+            lifecycleScope.launch {
+                val email = fragment_login_email.editText?.text
+                val password = fragment_login_email.editText?.text
+                viewModel.loginChannel.send(LoginSpec(email.toString(), password.toString()))
+            }
+        }
+
+        lifecycleScope.launch {
+            viewModel.loginFlow.collectLatest {
+                println(it)
+            }
+        }
     }
 
     class Arguments(fragment: LoginFragment) : CoreFragment.Arguments(fragment)
--- application/android/app/src/main/java/com/makentoshe/habrachan/application/android/screen/login/viewmodel/LoginViewModel.kt
+++ application/android/app/src/main/java/com/makentoshe/habrachan/application/android/screen/login/viewmodel/LoginViewModel.kt
@@ -2,14 +2,37 @@ package com.makentoshe.habrachan.application.android.screen.login.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.makentoshe.habrachan.application.android.AndroidUserSession
+import com.makentoshe.habrachan.application.android.screen.login.model.LoginSpec
+import com.makentoshe.habrachan.network.manager.LoginManager
+import com.makentoshe.habrachan.network.request.LoginRequest
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.receiveAsFlow
 
-class LoginViewModel() : ViewModel() {
+class LoginViewModel(
+    private val userSession: AndroidUserSession, private val loginManager: LoginManager<LoginRequest>
+) : ViewModel() {
 
+    val loginChannel = Channel<LoginSpec>()
 
+    val loginFlow = loginChannel.receiveAsFlow().map { spec ->
+        loginManager.request(userSession, spec.email, spec.password)
+    }.flowOn(Dispatchers.IO).map { request ->
+        loginManager.login(request)
+    }.onEach {  result ->
+        result.onSuccess { response ->
+            response.nativeResponse?.accessToken?.let { userSession.token = it }
+        }
+    }.flowOn(Dispatchers.IO)
 
-    class Factory() : ViewModelProvider.NewInstanceFactory() {
+    class Factory(
+        private val userSession: AndroidUserSession, private val loginManager: LoginManager<LoginRequest>
+    ) : ViewModelProvider.NewInstanceFactory() {
         @Suppress("UNCHECKED_CAST")
-        override fun <T : ViewModel?> create(modelClass: Class<T>) = LoginViewModel() as T
-
+        override fun <T : ViewModel?> create(modelClass: Class<T>) = LoginViewModel(userSession, loginManager) as T
     }
 }
