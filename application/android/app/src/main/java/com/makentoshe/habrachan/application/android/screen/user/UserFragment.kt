339bb59d4bee47654ea04dc10582930bd526ba4b
Makentoshe <mkliverout@gmail.com>
2021-04-15 16:26:52 +0300

    feat(android): Implement backward navigation in user screen

--- application/android/app/src/main/java/com/makentoshe/habrachan/application/android/screen/user/UserFragment.kt
+++ application/android/app/src/main/java/com/makentoshe/habrachan/application/android/screen/user/UserFragment.kt
@@ -9,6 +9,7 @@ import com.makentoshe.habrachan.R
 import com.makentoshe.habrachan.application.android.CoreFragment
 import com.makentoshe.habrachan.application.android.dp2px
 import com.makentoshe.habrachan.application.android.screen.user.model.UserAccount
+import com.makentoshe.habrachan.application.android.screen.user.navigation.UserNavigation
 import com.makentoshe.habrachan.application.android.screen.user.viewmodel.UserViewModel
 import com.makentoshe.habrachan.application.android.toRoundedDrawable
 import com.makentoshe.habrachan.entity.User
@@ -31,6 +32,7 @@ class UserFragment : CoreFragment() {
 
     override val arguments = Arguments(this)
     private val viewModel by inject<UserViewModel>()
+    private val navigation by inject<UserNavigation>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_user, container, false)
@@ -44,6 +46,8 @@ class UserFragment : CoreFragment() {
         when (val account = arguments.account) {
             is UserAccount.Me -> fragment_user_toolbar.title = account.login
         }
+
+        fragment_user_toolbar.setNavigationOnClickListener { navigation.back() }
 
         lifecycleScope.launch {
             viewModel.user.collectLatest { either ->
--- application/android/app/src/main/java/com/makentoshe/habrachan/application/android/screen/user/di/UserModule.kt
+++ application/android/app/src/main/java/com/makentoshe/habrachan/application/android/screen/user/di/UserModule.kt
@@ -6,7 +6,9 @@ import com.makentoshe.habrachan.application.android.arena.ContentArenaCache
 import com.makentoshe.habrachan.application.android.arena.GetUserArenaCache
 import com.makentoshe.habrachan.application.android.database.AndroidCacheDatabase
 import com.makentoshe.habrachan.application.android.di.ApplicationScope
+import com.makentoshe.habrachan.application.android.navigation.StackRouter
 import com.makentoshe.habrachan.application.android.screen.user.UserFragment
+import com.makentoshe.habrachan.application.android.screen.user.navigation.UserNavigation
 import com.makentoshe.habrachan.application.android.screen.user.viewmodel.UserViewModel
 import com.makentoshe.habrachan.application.core.arena.image.ContentArena
 import com.makentoshe.habrachan.application.core.arena.users.GetUserArena
@@ -20,15 +22,17 @@ import toothpick.ktp.delegate.inject
 
 annotation class UserScope
 
-class UserModule(fragment: UserFragment): Module() {
+class UserModule(fragment: UserFragment) : Module() {
 
     private val androidUserSession by inject<AndroidUserSession>()
     private val androidCacheDatabase by inject<AndroidCacheDatabase>()
     private val getUserManager by inject<GetUserManager<GetUserRequest>>()
     private val getContentManager by inject<GetContentManager>()
+    private val stackRouter by inject<StackRouter>()
 
     init {
         Toothpick.openScope(ApplicationScope::class).inject(this)
+        bind<UserNavigation>().toInstance(UserNavigation(stackRouter))
 
         val contentArenaCache = ContentArenaCache(androidCacheDatabase.contentDao(), fragment.requireContext().cacheDir)
         val getContentArena = ContentArena(getContentManager, contentArenaCache)
