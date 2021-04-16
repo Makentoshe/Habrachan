7d78a2f0d530388d435161bcf830c7c42bf44d12
Makentoshe <mkliverout@gmail.com>
2021-04-16 14:56:35 +0300

    feat(android): Add user specialization

--- application/android/app/src/main/java/com/makentoshe/habrachan/application/android/screen/user/UserFragment.kt
+++ application/android/app/src/main/java/com/makentoshe/habrachan/application/android/screen/user/UserFragment.kt
@@ -71,6 +71,7 @@ class UserFragment : CoreFragment() {
         fragment_user_toolbar.title = user.login
         fragment_user_fullname.text = user.fullname
         fragment_user_registered.text = SimpleDateFormat("MMMM dd, YYYY").format(user.timeRegistered)
+        fragment_user_speciality.text = user.speciality
 
         fragment_user_counters_karma.text = user.score.toString()
         fragment_user_counters_rating.text = user.rating.toString()
