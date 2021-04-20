48f011840d42eb8d2d8eb217a9644899cc7efac9
Makentoshe <mkliverout@gmail.com>
2021-04-20 16:20:49 +0300

    feat(application): override toString for ArenaException

--- application/core/src/main/kotlin/com/makentoshe/habrachan/application/core/arena/ArenaException.kt
+++ application/core/src/main/kotlin/com/makentoshe/habrachan/application/core/arena/ArenaException.kt
@@ -1,7 +1,9 @@
 package com.makentoshe.habrachan.application.core.arena
 
 class ArenaException(
-    val sourceException: Throwable? = null,
-    val cacheException: Throwable? = null,
-    override val message: String? = null
-): Throwable()
+    val sourceException: Throwable? = null, val cacheException: Throwable? = null, override val message: String? = null
+) : Throwable() {
+    override fun toString(): String {
+        return "{\nsource=$sourceException,\ncache=$cacheException,\nmessage=$message\n}"
+    }
+}
