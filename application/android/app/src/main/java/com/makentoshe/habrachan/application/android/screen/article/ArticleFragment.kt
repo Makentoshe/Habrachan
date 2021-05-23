a7b288669e129d974220fa315586fb1bceb93abe
Makentoshe <mkliverout@gmail.com>
2021-05-23 19:24:43 +0300

    fix(android): fix article screen state after application releasing from the memory

--- application/android/app/src/main/java/com/makentoshe/habrachan/application/android/screen/article/ArticleFragment.kt
+++ application/android/app/src/main/java/com/makentoshe/habrachan/application/android/screen/article/ArticleFragment.kt
@@ -14,9 +14,13 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.makentoshe.habrachan.R
 import com.makentoshe.habrachan.application.android.*
+import com.makentoshe.habrachan.application.android.analytics.Analytics
+import com.makentoshe.habrachan.application.android.analytics.LogAnalytic
+import com.makentoshe.habrachan.application.android.analytics.event.analyticEvent
 import com.makentoshe.habrachan.application.android.screen.article.model.*
 import com.makentoshe.habrachan.application.android.screen.article.navigation.ArticleNavigation
 import com.makentoshe.habrachan.application.android.screen.article.viewmodel.ArticleViewModel2
+import com.makentoshe.habrachan.application.android.screen.user.UserFragment
 import com.makentoshe.habrachan.network.response.GetArticleResponse2
 import com.makentoshe.habrachan.network.response.GetContentResponse
 import kotlinx.android.synthetic.main.fragment_article.*
@@ -28,11 +32,13 @@ import toothpick.ktp.delegate.inject
 
 class ArticleFragment : CoreFragment(), HabrachanWebViewClientListener {
 
-    companion object {
+    companion object: Analytics(LogAnalytic()) {
 
         fun build(articleId: Int) = ArticleFragment().apply {
             arguments.articleId = articleId
         }
+
+        private const val VIEW_MODEL_STATE_KEY = "ViewModel"
     }
 
     override val arguments = Arguments(this)
@@ -57,7 +63,9 @@ class ArticleFragment : CoreFragment(), HabrachanWebViewClientListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         exceptionController = ExceptionController(ExceptionViewHolder(fragment_article_exception))
 
-        if (savedInstanceState == null) lifecycleScope.launch {
+        val wasViewModelRecreated = viewModel2.toString() != savedInstanceState?.getString(VIEW_MODEL_STATE_KEY)
+        if (savedInstanceState == null || wasViewModelRecreated) lifecycleScope.launch {
+            capture(analyticEvent(this@ArticleFragment.javaClass.simpleName, arguments.articleId.toString()))
             viewModel2.articleSpecChannel.send(ArticleViewModel2.ArticleSpec(arguments.articleId))
         }
         lifecycleScope.launch {
@@ -168,6 +176,11 @@ class ArticleFragment : CoreFragment(), HabrachanWebViewClientListener {
         fragment_article_avatar_image.visibility = View.VISIBLE
 
         fragment_article_avatar_progress.visibility = View.GONE
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(VIEW_MODEL_STATE_KEY, viewModel2.toString())
     }
 
     override fun onWebReceivedError(
