0da72c5ebb4fc737b0ec7e608f62f9fb28a4f5ce
Makentoshe <mkliverout@gmail.com>
2021-05-28 15:49:50 +0300

    feat(network): add additional tests NativeGetArticlesManagerTest

---
+++ network/network-native/network-native-test/src/test/kotlin/com/makentoshe/habrachan/network/manager/NativeUnitTest.kt
@@ -0,0 +1,6 @@
+package com.makentoshe.habrachan.network.manager
+
+abstract class NativeUnitTest {
+    val client = "85cab69095196f3.89453480"
+    val api = "173984950848a2d27c0cc1c76ccf3d6d3dc8255b"
+}
