8af6d81ba8e5d5f38cdf9e237e6e09391e9a8605
Makentoshe <mkliverout@gmail.com>
2021-05-30 15:58:08 +0300

    feat(network): add vote article manager for a native api

--- network/network-native/network-native-test/src/test/kotlin/com/makentoshe/habrachan/network/manager/NativeVoteArticleManagerTest.kt
+++ network/network-native/network-native-test/src/test/kotlin/com/makentoshe/habrachan/network/manager/NativeVoteArticleManagerTest.kt
@@ -3,8 +3,10 @@ package com.makentoshe.habrachan.network.manager
 import com.makentoshe.habrachan.entity.articleId
 import com.makentoshe.habrachan.network.deserializer.NativeVoteArticleDeserializer
 import com.makentoshe.habrachan.network.request.ArticleVote
+import com.makentoshe.habrachan.network.userSession
 import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
+import org.junit.Ignore
 import org.junit.Test
 
 class NativeVoteArticleManagerTest: NativeUnitTest() {
@@ -13,8 +15,11 @@ class NativeVoteArticleManagerTest: NativeUnitTest() {
 
     @Test
     @Ignore
-    fun sas() = runBlocking {
-        val request = manager.request(userSession, articleId(442440), ArticleVote.UP)
+    fun success() = runBlocking {
+        val userSession = userSession(client, api, "")
+        val request = manager.request(userSession, articleId(559980), ArticleVote.UP)
         val response = manager.vote(request)
+
+        println(response)
     }
 }
