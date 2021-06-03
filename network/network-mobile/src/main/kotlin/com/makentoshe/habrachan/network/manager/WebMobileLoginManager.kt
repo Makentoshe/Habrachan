8284ee52e8add7cec8d4ce0f305d0702934c0397
Makentoshe <mkliverout@gmail.com>
2021-06-04 02:35:14 +0300

    feat(network-mobile): implement logging using mobile api and a web tool

--- network/network-mobile/src/main/kotlin/com/makentoshe/habrachan/network/manager/WebMobileLoginManager.kt
+++ network/network-mobile/src/main/kotlin/com/makentoshe/habrachan/network/manager/WebMobileLoginManager.kt
@@ -2,53 +2,74 @@ package com.makentoshe.habrachan.network.manager
 
 import com.makentoshe.habrachan.network.UserSession
 import com.makentoshe.habrachan.network.api.MobileLoginApi
-import com.makentoshe.habrachan.network.request.MobileLoginRequest
-import com.makentoshe.habrachan.network.response.LoginResponse
-import okhttp3.OkHttpClient
+import com.makentoshe.habrachan.network.fold
+import com.makentoshe.habrachan.network.request.WebMobileLoginRequest
+import com.makentoshe.habrachan.network.response.WebMobileLoginResponse
+import okhttp3.*
 import retrofit2.Retrofit
 
 /**
- * Performs a login using mobile api.
+ * This manager uses an external web tool for logging.
  *
  * Should use with ui, that works like a web browser, e.g. WebView.
  */
 class WebMobileLoginManager private constructor(
-    private val loginApi: MobileLoginApi,
+    private val loginApi: MobileLoginApi, private val client: OkHttpClient, private val cookieJar: CustomCookieJar
 ) {
 
-    fun request(userSession: UserSession, email: String, password: String): MobileLoginRequest {
-        return MobileLoginRequest(userSession, email, password)
+    fun request(userSession: UserSession, external: suspend (String) -> String): WebMobileLoginRequest {
+        return WebMobileLoginRequest(userSession, external)
     }
 
     @Suppress("BlockingMethodInNonBlockingContext")
-    suspend fun login(request: MobileLoginRequest, htmlProcess: suspend (String) -> LoginResponse.MobileResponse): Unit {
+    suspend fun login(request: WebMobileLoginRequest): Result<WebMobileLoginResponse> {
         try {
             // get cookies. consumer, state and referer
             val cookieResponse = loginApi.getLoginCookies().execute()
             val referer = cookieResponse.raw().request.url
             val state = referer.queryParameter("state") ?: ""
             val consumer = referer.queryParameter("consumer") ?: ""
-            val url = "https://account.habr.com/login/?state=$state&consumer=$consumer&hl=${request.userSession.habrLanguage}"
+            val loginScreenUrl =
+                "https://account.habr.com/login/?state=$state&consumer=$consumer&hl=${request.userSession.habrLanguage}"
 
-            val response = htmlProcess(url)
-            println(response)
+            val loginScreenPassedUrl = request.external(loginScreenUrl)
+            val loginScreenPassedRequest = Request.Builder().url(loginScreenPassedUrl).build()
+            val loginScreenPassedResponse = client.newCall(loginScreenPassedRequest).execute()
+
+            val string = loginScreenPassedResponse.body?.string().toString()
+            return loginScreenPassedResponse.fold({
+                Result.success(WebMobileLoginResponse(request, cookieJar.cookies.flatMap { it.value }, string))
+            }, {
+                Result.failure(Exception(it.string()))
+            })
         } catch (exception: Exception) {
-            println(exception)
+            return Result.failure(exception)
         }
     }
 
-    /**
-     * pass user manager for second request that allows to return user in login response
-     * if userManager == null so user is also will be null
-     */
     class Builder(client: OkHttpClient) {
 
+        private val cookieJar = CustomCookieJar()
         private val baseUrl = "https://account.habr.com"
-        private val client = client.newBuilder().build()
+        private val client = client.newBuilder().cookieJar(cookieJar).build()
 
         private fun getRetrofit() = Retrofit.Builder().client(client).baseUrl(baseUrl).build()
 
-        fun build() =
-            WebMobileLoginManager(getRetrofit().create(MobileLoginApi::class.java))
+        fun build() = WebMobileLoginManager(getRetrofit().create(MobileLoginApi::class.java), client, cookieJar)
+    }
+
+    private class CustomCookieJar : CookieJar {
+
+        val cookies = HashMap<HttpUrl, ArrayList<Cookie>>()
+
+        override fun loadForRequest(url: HttpUrl): List<Cookie> {
+            return cookies.values.flatten()
+        }
+
+        override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
+            this.cookies[url] = (this.cookies[url] ?: ArrayList()).apply {
+                addAll(cookies)
+            }
+        }
     }
 }
---
+++ network/network-mobile/src/main/kotlin/com/makentoshe/habrachan/network/response/WebMobileLoginResponse.kt
@@ -0,0 +1,14 @@
+package com.makentoshe.habrachan.network.response
+
+import com.makentoshe.habrachan.network.request.WebMobileLoginRequest
+import okhttp3.Cookie
+
+/**
+ * Mobile manager main response.
+ * This login response will be useful only for mobile managers.
+ */
+data class WebMobileLoginResponse(
+    val request: WebMobileLoginRequest,
+    val cookies: List<Cookie>,
+    val raw: String
+)
