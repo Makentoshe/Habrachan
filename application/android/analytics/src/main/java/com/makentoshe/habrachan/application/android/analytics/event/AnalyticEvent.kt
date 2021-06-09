25af3f39df1942889fa008a5722581061d7a0256
Makentoshe <mkliverout@gmail.com>
2021-06-09 20:14:43 +0300

    feat(android, analytics): add 2 additional functions for AnalyticEvent creation

--- application/android/analytics/src/main/java/com/makentoshe/habrachan/application/android/analytics/event/AnalyticEvent.kt
+++ application/android/analytics/src/main/java/com/makentoshe/habrachan/application/android/analytics/event/AnalyticEvent.kt
@@ -1,5 +1,7 @@
 package com.makentoshe.habrachan.application.android.analytics.event
 
+import kotlin.reflect.KClass
+
 interface AnalyticEvent {
     val eventTitle: String
     val eventMessage: String
@@ -9,3 +11,7 @@ fun analyticEvent(title: String, message: String) = object : AnalyticEvent {
     override val eventTitle: String = title
     override val eventMessage: String = message
 }
+
+fun analyticEvent(`class`: KClass<Any>, message: String) = analyticEvent(`class`.simpleName.toString(), message)
+
+fun Any.analyticEvent(message: String) = analyticEvent(this::class.java.simpleName, message)
