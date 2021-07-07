3f3d883485c00fa6a7defd404ed6ea80e207fdf6
Makentoshe <mkliverout@gmail.com>
2021-07-08 01:07:53 +0300

    feat(android): add factory class for BlockViewHolder

--- application/android/common/common-comment/src/main/java/com/makentoshe/habrachan/application/android/common/comment/BlockViewHolder.kt
+++ application/android/common/common-comment/src/main/java/com/makentoshe/habrachan/application/android/common/comment/BlockViewHolder.kt
@@ -1,13 +1,23 @@
 package com.makentoshe.habrachan.application.android.common.comment
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class BlockViewHolder(view: View): RecyclerView.ViewHolder(view) {
+class BlockViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val context: Context = view.context
     val blockText: TextView = itemView.findViewById(R.id.layout_comment_block_text)
     val levelView: ViewGroup = view.findViewById(R.id.layout_comment_block_level)
+
+    class Factory {
+
+        fun create(context: Context, parent: ViewGroup? = null): CommentViewHolder {
+            val inflater = LayoutInflater.from(context)
+            val view = inflater.inflate(R.layout.layout_comment_block, parent, false)
+            return CommentViewHolder(view)
+        }
+    }
 }
