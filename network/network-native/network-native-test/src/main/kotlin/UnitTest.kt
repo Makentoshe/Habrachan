58d03798f028db91a4b74ed0f97571f1e54f153e
Makentoshe <mkliverout@gmail.com>
2021-07-15 19:37:19 +0300

    test: implement local.properties reading for tests purposes

--- network/network-native/network-native-test/src/main/kotlin/UnitTest.kt
+++ network/network-native/network-native-test/src/main/kotlin/UnitTest.kt
@@ -1,11 +1,28 @@
 import com.makentoshe.habrachan.network.userSession
+import java.io.File
 import java.io.FileNotFoundException
+import java.util.*
 
 abstract class UnitTest {
 
-    val userSession = userSession("", "")
+    protected val properties = getLocalProperties(File("").absoluteFile)
+
+    protected val client = properties.getProperty("client")
+    protected val api = properties.getProperty("api")
+    protected val token = properties.getProperty("token")
+
+    protected val userSession = userSession("", "")
 
     fun getResourceString(path: String): String {
         return this::class.java.classLoader.getResource(path)?.readText() ?: throw FileNotFoundException(path)
+    }
+
+    private fun getLocalProperties(file: File) : Properties {
+        val files = file.listFiles()?.map { it.name } ?: emptyList()
+        return if (files.contains("local.properties")) Properties().apply {
+            load(File(file, "local.properties").inputStream())
+        } else {
+            getLocalProperties(file.parentFile)
+        }
     }
 }
