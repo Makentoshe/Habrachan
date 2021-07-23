ebbb633d6c419da2b67138b08fb577e9e91d8b00
Makentoshe <mkliverout@gmail.com>
2021-07-23 20:35:27 +0300

    build(gradle): update gradle android plugin to 4.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,13 +11,12 @@ allprojects {
 }
 
 buildscript {
-    val kotlin_version by extra("1.5.10")
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.2")
+        classpath("com.android.tools.build:gradle:4.2.2")
         classpath(kotlin("gradle-plugin", version = "1.4.20"))
 
         // NOTE: Do not place your application dependencies here; they belong
