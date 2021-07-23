470f8f0f8b5438c5c60b706be33a7c55ce559171
Makentoshe <mkliverout@gmail.com>
2021-07-23 21:04:16 +0300

    build(gradle): move kotlin to 1.4.32

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.4.20"
+    id("org.jetbrains.kotlin.jvm") version "1.4.32"
 }
 
 allprojects {
@@ -15,7 +15,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.2.2")
-        classpath(kotlin("gradle-plugin", version = "1.4.20"))
+        classpath(kotlin("gradle-plugin", version = "1.4.32"))
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
