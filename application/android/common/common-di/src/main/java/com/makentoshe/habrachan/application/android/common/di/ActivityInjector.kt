f5b106e9a451b310e6dc65df867b8ad3ac412c11
Makentoshe <mkliverout@gmail.com>
2021-07-26 14:57:29 +0300

    feat(application, android, common, di): add abstract injector for activities

---
+++ application/android/common/common-di/src/main/java/com/makentoshe/habrachan/application/android/common/di/ActivityInjector.kt
@@ -0,0 +1,25 @@
+package com.makentoshe.habrachan.application.android.common.di
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import kotlin.reflect.KClass
+
+abstract class ActivityInjector<T: FragmentActivity>(
+    private val `class`: KClass<T>,
+    private val action: T.(Bundle?) -> Unit
+) : Application.ActivityLifecycleCallbacks {
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        if (activity::class != `class`) return
+        action.invoke(activity as T, savedInstanceState)
+    }
+
+    override fun onActivityStarted(activity: Activity?) = Unit
+    override fun onActivityPaused(activity: Activity?) = Unit
+    override fun onActivityResumed(activity: Activity?) = Unit
+    override fun onActivityStopped(activity: Activity?) = Unit
+    override fun onActivityDestroyed(activity: Activity?) = Unit
+    override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) = Unit
+}
