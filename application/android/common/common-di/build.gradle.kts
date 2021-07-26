4058e7bcd395b7175cf5de09ef3c08057ee5b086
Makentoshe <mkliverout@gmail.com>
2021-07-26 14:57:29 +0300

    feat(application, android, common, di): add abstract injector for activities

---
+++ application/android/common/common-di/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("android.extensions")
+    id("kotlin-kapt")
+}
+
+android {
+    packagingOptions {
+        exclude("META-INF/DEPENDENCIES")
+        exclude("META-INF/LICENSE*")
+        exclude("META-INF/NOTICE*")
+        exclude("META-INF/*.kotlin_module")
+    }
+
+    compileSdkVersion(29)
+    defaultConfig {
+        minSdkVersion(21)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFile("consumer-rules.pro")
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    implementation(kotlin("stdlib"))
+
+    val androidxCoreVersion = properties["version.androidx.core"]
+    implementation("androidx.core:core-ktx:$androidxCoreVersion")
+
+    val androidxAppcompatVersion = properties["version.androidx.appcompat"]
+    implementation("androidx.appcompat:appcompat:$androidxAppcompatVersion")
+
+    testImplementation("junit:junit:4.12")
+
+    val runner = properties["version.androidx.test.runner"]
+    androidTestImplementation("androidx.test:runner:$runner")
+
+    val espresso = properties["version.androidx.test.espresso"]
+    androidTestImplementation("androidx.test.espresso:espresso-core:$espresso")
+}
