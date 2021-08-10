7ddf0d1883fe7b031356568b0fa1b6aaf42910e6
Makentoshe <mkliverout@gmail.com>
2021-08-10 17:59:30 +0300

    fix(app): fix Launcher deeplinking for corporate blogs

--- application/android/app/src/main/java/com/makentoshe/habrachan/application/android/Launcher.kt
+++ application/android/app/src/main/java/com/makentoshe/habrachan/application/android/Launcher.kt
@@ -2,6 +2,9 @@ package com.makentoshe.habrachan.application.android
 
 import android.content.Intent
 import android.net.Uri
+import com.makentoshe.habrachan.application.android.analytics.Analytics
+import com.makentoshe.habrachan.application.android.analytics.LogAnalytic
+import com.makentoshe.habrachan.application.android.analytics.event.analyticEvent
 import com.makentoshe.habrachan.application.android.screen.article.navigation.ArticleScreen
 import com.makentoshe.habrachan.application.android.screen.comments.navigation.ArticleCommentsScreen
 import com.makentoshe.habrachan.application.android.screen.comments.navigation.DiscussionCommentsScreen
@@ -12,8 +15,13 @@ import ru.terrakok.cicerone.Screen
 
 /**
  * Class resolves startup activity intent and returns a screen to launch first
+ *
+ * TODO(high): mb hide parsing with kotlin dsl
+ * TODO(low): mb replace with parser combinator
  */
 class Launcher(private val defaultScreen: Screen) {
+
+    companion object : Analytics(LogAnalytic())
 
     /** Resolve intent from activity */
     fun launch(intent: Intent) = when (intent.action) {
@@ -27,13 +35,14 @@ class Launcher(private val defaultScreen: Screen) {
 
     /** Try to resolve deeplink intent. If resolve failed default screen launches instead */
     fun deeplink(intent: Intent): Screen {
+        capture(analyticEvent { "Deeplinking: ${intent.data}" })
         return deeplink(0, intent.data ?: return default())
     }
 
     /** Internal deeplink resolving */
     private fun deeplink(depth: Int, uri: Uri): Screen = try {
         when (uri.pathSegments[depth]) {
-            "post" -> article(depth + 1, uri)
+            "post", "blog" -> article(depth + 1, uri)
             "users", "user" -> users(depth + 1, uri)
             else -> deeplink(depth + 1, uri)
         }
