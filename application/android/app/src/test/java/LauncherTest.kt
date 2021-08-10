33bcc1801b00b524ea93c64dee5d4683a3ad61d6
Makentoshe <mkliverout@gmail.com>
2021-08-10 17:35:08 +0300

    test(app): update LauncherTest tests

--- application/android/app/src/test/java/LauncherTest.kt
+++ application/android/app/src/test/java/LauncherTest.kt
@@ -1,3 +1,4 @@
+import android.content.Intent
 import android.net.Uri
 import com.makentoshe.habrachan.application.android.Launcher
 import com.makentoshe.habrachan.application.android.screen.articles.navigation.ArticlesFlowScreen
@@ -5,20 +6,20 @@ import com.makentoshe.habrachan.application.android.screen.comments.navigation.A
 import com.makentoshe.habrachan.application.android.screen.comments.navigation.DiscussionCommentsScreen
 import com.makentoshe.habrachan.application.android.screen.user.model.UserAccount
 import com.makentoshe.habrachan.application.android.screen.user.navigation.UserScreen
-import org.junit.Assert.assertEquals
+import junit.framework.TestCase
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
-class LauncherTest {
+class LauncherTest : TestCase() {
 
     private val launcher = Launcher(ArticlesFlowScreen())
 
     @Test
     fun testShouldOpenDiscussionCommentsScreen1() {
-        val uri = Uri.parse("https://habr.com/en/post/571418/#comment_23344290")
-        val screen = launcher.deeplink(0, uri) as DiscussionCommentsScreen
+        val intent = intent(Uri.parse("https://habr.com/en/post/571418/#comment_23344290"))
+        val screen = launcher.deeplink(intent) as DiscussionCommentsScreen
 
         assertEquals(571418, screen.articleId)
         assertEquals(23344290, screen.commentId)
@@ -26,8 +27,8 @@ class LauncherTest {
 
     @Test
     fun testShouldOpenDiscussionCommentsScreen2() {
-        val uri = Uri.parse("https://habr.com/en/post/571418/comments/#comment_23344290")
-        val screen = launcher.deeplink(0, uri) as DiscussionCommentsScreen
+        val intent = intent(Uri.parse("https://habr.com/en/post/571418/comments/#comment_23344290"))
+        val screen = launcher.deeplink(intent) as DiscussionCommentsScreen
 
         assertEquals(571418, screen.articleId)
         assertEquals(23344290, screen.commentId)
@@ -35,25 +36,27 @@ class LauncherTest {
 
     @Test
     fun testShouldOpenArticleCommentsScreen() {
-        val uri = Uri.parse("https://habr.com/en/post/571418/comments/")
-        val screen = launcher.deeplink(0, uri) as ArticleCommentsScreen
+        val intent = intent(Uri.parse("https://habr.com/en/post/571418/comments/"))
+        val screen = launcher.deeplink(intent) as ArticleCommentsScreen
 
         assertEquals(571418, screen.articleId.articleId)
     }
 
     @Test
     fun testShouldOpenUserScreen1() {
-        val uri = Uri.parse("https://habr.com/en/users/makentoshe/")
-        val screen = launcher.deeplink(0, uri) as UserScreen
+        val intent = intent(Uri.parse("https://habr.com/en/users/makentoshe/"))
+        val screen = launcher.deeplink(intent) as UserScreen
 
         assertEquals("makentoshe", (screen.account as UserAccount.User).login)
     }
 
     @Test
     fun testShouldOpenUserScreen2() {
-        val uri = Uri.parse("https://habr.com/users/makentoshe/")
-        val screen = launcher.deeplink(0, uri) as UserScreen
+        val intent = intent(Uri.parse("https://habr.com/users/makentoshe/"))
+        val screen = launcher.deeplink(intent) as UserScreen
 
         assertEquals("makentoshe", (screen.account as UserAccount.User).login)
     }
+
+    private fun intent(uri: Uri) = Intent(Intent.ACTION_VIEW, uri)
 }
