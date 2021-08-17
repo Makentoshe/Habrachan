6f5114e2a1e966fa9cfd74b143fec2bf8c9f0bd3
Makentoshe <mkliverout@gmail.com>
2021-08-17 20:34:51 +0300

    feat(application, android, app): add providers for ArticleCommentsArena instances

---
+++ application/android/app/src/main/java/com/makentoshe/habrachan/application/android/screen/comments/di/provider/ArticleCommentsArenaProvider.kt
@@ -0,0 +1,26 @@
+package com.makentoshe.habrachan.application.android.screen.comments.di.provider
+
+import com.makentoshe.habrachan.application.android.common.arena.ArticleCommentsArenaCache
+import com.makentoshe.habrachan.application.common.arena.comments.ArticleCommentsArena
+import com.makentoshe.habrachan.network.manager.GetArticleCommentsManager
+import com.makentoshe.habrachan.network.request.GetArticleCommentsRequest
+import javax.inject.Inject
+import javax.inject.Provider
+
+class SourceFirstArticleCommentsArenaProvider @Inject constructor(
+    private val manager: GetArticleCommentsManager<GetArticleCommentsRequest>,
+    private val arenaCache: ArticleCommentsArenaCache,
+) : Provider<ArticleCommentsArena> {
+    override fun get(): ArticleCommentsArena {
+        return ArticleCommentsArena.Factory(manager, arenaCache).sourceFirstArena()
+    }
+}
+
+class CacheFirstArticleCommentsArenaProvider @Inject constructor(
+    private val manager: GetArticleCommentsManager<GetArticleCommentsRequest>,
+    private val arenaCache: ArticleCommentsArenaCache,
+) : Provider<ArticleCommentsArena> {
+    override fun get(): ArticleCommentsArena {
+        return ArticleCommentsArena.Factory(manager, arenaCache).cacheFirstArena()
+    }
+}
