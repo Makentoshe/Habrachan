3cfa32315af07be84a5904fb167a8b2105dd7898
Makentoshe <mkliverout@gmail.com>
2021-08-17 18:58:58 +0300

    feat(application, android, common, arena, comments): implement arena caches for article comments

---
+++ application/android/common/arena/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("android.extensions")
+    id("kotlin-kapt")
+}
+
+android {
+    packagingOptions {
+        exclude("META-INF/DEPENDENCIES")
+        exclude("META-INF/LICENSE*")
+        exclude("META-INF/NOTICE*")
+        exclude("META-INF/*.kotlin_module")
+    }
+
+    compileSdkVersion(29)
+    defaultConfig {
+        minSdkVersion(21)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFile("consumer-rules.pro")
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
+
+dependencies {
+
+    implementation(kotlin("stdlib"))
+
+    implementation(project(":entity"))
+    implementation(project(":network"))
+    implementation(project(":application:common:arena:comments"))
+    implementation(project(":application:android:database"))
+    implementation(project(":application:android:analytics"))
+
+    implementation(project(":functional"))
+
+    // Toothpick
+    // https://github.com/stephanenicolas/toothpick
+    val toothpickVersion = dependency.version.toothpick
+    implementation("com.github.stephanenicolas.toothpick:ktp:$toothpickVersion")
+    kapt("com.github.stephanenicolas.toothpick:toothpick-compiler:$toothpickVersion")
+    implementation("com.github.stephanenicolas.toothpick:smoothie:$toothpickVersion")
+    implementation("com.github.stephanenicolas.toothpick:smoothie-lifecycle-ktp:$toothpickVersion")
+    testImplementation("com.github.stephanenicolas.toothpick:toothpick-testing-junit5:$toothpickVersion")
+
+    val coreVersion = dependency.version.androidCore
+    implementation("androidx.core:core-ktx:$coreVersion")
+
+    val appcompatVersion = dependency.version.androidAppCompat
+    implementation("androidx.appcompat:appcompat:$appcompatVersion")
+}
---
+++ application/android/common/arena/src/main/java/com/makentoshe/habrachan/application/android/common/arena/ArticleCommentsArenaCache.kt
@@ -0,0 +1,57 @@
+package com.makentoshe.habrachan.application.android.common.arena
+
+import com.makentoshe.habrachan.application.android.analytics.Analytics
+import com.makentoshe.habrachan.application.android.analytics.LogAnalytic
+import com.makentoshe.habrachan.application.android.analytics.event.analyticEvent
+import com.makentoshe.habrachan.application.android.database.AndroidCacheDatabase
+import com.makentoshe.habrachan.application.android.database.record.CommentRecord
+import com.makentoshe.habrachan.application.common.arena.ArenaCache
+import com.makentoshe.habrachan.application.common.arena.ArenaStorageException
+import com.makentoshe.habrachan.entity.Comment
+import com.makentoshe.habrachan.functional.Result
+import com.makentoshe.habrachan.network.request.GetArticleCommentsRequest
+import com.makentoshe.habrachan.network.response.GetArticleCommentsResponse
+import javax.inject.Inject
+
+class ArticleCommentsArenaCache @Inject constructor(
+    private val database: AndroidCacheDatabase,
+) : ArenaCache<GetArticleCommentsRequest, GetArticleCommentsResponse> {
+
+    companion object : Analytics(LogAnalytic()) {
+        private const val limit = 3000
+        private const val step = 500
+    }
+
+    private val commentDao = database.commentDao()
+
+    override fun fetch(key: GetArticleCommentsRequest): Result<GetArticleCommentsResponse> = try {
+        val records = database.commentDao().getByArticleId(key.articleId.articleId)
+        capture(analyticEvent { "Fetched ${records.size} comments by key: $key" })
+        if (records.isEmpty()) {
+            Result.failure(ArenaStorageException("CommentsArenaCache"))
+        } else {
+            Result.success(getArticleCommentsResponse(key, records.map { it.toComment() }))
+        }
+    } catch (exception: Exception) {
+        capture(analyticEvent { "Could not fetch comments: $exception" })
+        Result.failure(ArenaStorageException("CommentsArenaCache").initCause(exception))
+    }
+
+    override fun carry(key: GetArticleCommentsRequest, value: GetArticleCommentsResponse) {
+        if (commentDao.count() > limit) capture(analyticEvent {
+            // TODO implement removing oldest elements
+            "TODO: Removing oldest $step elements from cache"
+        })
+
+        // TODO if the element already placed in the database - reset it lifecycle index
+        capture(analyticEvent { "Carry ${value.data.size} comments to cache with key: $key" })
+        value.data.map { comment -> CommentRecord(key.articleId.articleId, comment) }.forEach { record ->
+            commentDao.insert(record)
+        }
+    }
+
+    private fun getArticleCommentsResponse(request: GetArticleCommentsRequest, data: List<Comment>) = object : GetArticleCommentsResponse {
+        override val request: GetArticleCommentsRequest = request
+        override val data: List<Comment> = data
+    }
+}
