e43be92baf0cef4678de6bd24845e49f6145010e
Makentoshe <mkliverout@gmail.com>
2021-08-24 13:33:21 +0300

    feat(application, android, common, avatar): implement providers for avatar arena instances

---
+++ application/android/app/src/main/java/com/makentoshe/habrachan/application/android/screen/comments/di/provider/AvatarArenaProvider.kt
@@ -0,0 +1,25 @@
+package com.makentoshe.habrachan.application.android.screen.comments.di.provider
+
+import com.makentoshe.habrachan.application.android.common.arena.AvatarArenaCache
+import com.makentoshe.habrachan.application.common.arena.content.ContentArena
+import com.makentoshe.habrachan.network.GetContentManager
+import javax.inject.Inject
+import javax.inject.Provider
+
+class SourceFirstAvatarArenaProvider @Inject constructor(
+    private val manager: GetContentManager,
+    private val arenaStorage: AvatarArenaCache,
+) : Provider<ContentArena> {
+    override fun get(): ContentArena {
+        return ContentArena.Factory(manager, arenaStorage).sourceFirstArena()
+    }
+}
+
+class CacheFirstAvatarArenaProvider @Inject constructor(
+    private val manager: GetContentManager,
+    private val arenaStorage: AvatarArenaCache,
+) : Provider<ContentArena> {
+    override fun get(): ContentArena {
+        return ContentArena.Factory(manager, arenaStorage).cacheFirstArena()
+    }
+}
