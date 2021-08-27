df6c31f4329a1274d25a01bfdb60f2f0cd5ddf0c
Makentoshe <mkliverout@gmail.com>
2021-08-27 10:48:25 +0300

    feat(application, android, screen, comments, dispatch): implement providing CommentId instance for displaying a comment body

--- application/android/app/src/main/java/com/makentoshe/habrachan/application/android/navigation/DispatchCommentsScreenNavigatorImpl.kt
+++ application/android/app/src/main/java/com/makentoshe/habrachan/application/android/navigation/DispatchCommentsScreenNavigatorImpl.kt
@@ -3,12 +3,13 @@ package com.makentoshe.habrachan.application.android.navigation
 import com.makentoshe.habrachan.application.android.common.navigation.StackRouter
 import com.makentoshe.habrachan.application.android.common.navigation.navigator.DispatchCommentsScreenNavigator
 import com.makentoshe.habrachan.application.android.screen.comments.dispatch.navigation.DispatchCommentsScreen
+import com.makentoshe.habrachan.entity.CommentId
 import javax.inject.Inject
 
 class DispatchCommentsScreenNavigatorImpl @Inject constructor(
     private val router: StackRouter
 ): DispatchCommentsScreenNavigator {
-    override fun toDispatchScreen() {
-        router.stack(DispatchCommentsScreen())
+    override fun toDispatchScreen(commentId: CommentId) {
+        router.stack(DispatchCommentsScreen(commentId))
     }
 }
--- application/android/screen/comments/dispatch/src/main/java/com/makentoshe/habrachan/application/android/screen/comments/dispatch/navigation/DispatchCommentsScreen.kt
+++ application/android/screen/comments/dispatch/src/main/java/com/makentoshe/habrachan/application/android/screen/comments/dispatch/navigation/DispatchCommentsScreen.kt
@@ -2,10 +2,11 @@ package com.makentoshe.habrachan.application.android.screen.comments.dispatch.na
 
 import androidx.fragment.app.Fragment
 import com.makentoshe.habrachan.application.android.screen.comments.dispatch.DispatchCommentsFragment
+import com.makentoshe.habrachan.entity.CommentId
 import ru.terrakok.cicerone.android.support.SupportAppScreen
 
-class DispatchCommentsScreen : SupportAppScreen() {
+class DispatchCommentsScreen(private val commentId: CommentId) : SupportAppScreen() {
     override fun getFragment(): Fragment {
-        return DispatchCommentsFragment.build()
+        return DispatchCommentsFragment.build(commentId)
     }
 }
