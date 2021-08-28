cc32d2833ea05a04c4f5c3ab8acbb77a8b8d122c
Makentoshe <mkliverout@gmail.com>
2021-08-28 11:57:41 +0300

    feat(application, android, common, comments): add @Inject annotation for VoteCommentViewModelProvider constructor

--- application/android/common/common-comment/src/main/java/com/makentoshe/habrachan/application/android/common/comment/viewmodel/VoteCommentViewModelProvider.kt
+++ application/android/common/common-comment/src/main/java/com/makentoshe/habrachan/application/android/common/comment/viewmodel/VoteCommentViewModelProvider.kt
@@ -3,8 +3,9 @@ package com.makentoshe.habrachan.application.android.common.comment.viewmodel
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.makentoshe.habrachan.application.android.common.di.provider.ViewModelKeyProvider
+import javax.inject.Inject
 
-class VoteCommentViewModelProvider(
+class VoteCommentViewModelProvider @Inject constructor(
     private val fragment: Fragment,
     private val factory: VoteCommentViewModel.Factory,
 ) : ViewModelKeyProvider<VoteCommentViewModel> {
