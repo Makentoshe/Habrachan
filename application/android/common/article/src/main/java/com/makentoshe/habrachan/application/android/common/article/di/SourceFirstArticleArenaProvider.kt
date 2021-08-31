7091bd0e5b228a4379f716a218a7bbe24711c011
Makentoshe <mkliverout@gmail.com>
2021-08-31 11:10:13 +0300

    feat(application, android, common, article): implement providers for ArticleArena for both implementations: cache first and source first
    These providers allow to reduce the boilerplate bindings in di modules

---
+++ application/android/common/article/src/main/java/com/makentoshe/habrachan/application/android/common/article/di/SourceFirstArticleArenaProvider.kt
@@ -0,0 +1,17 @@
+package com.makentoshe.habrachan.application.android.common.article.di
+
+import com.makentoshe.habrachan.application.android.common.article.arena.ArticleArenaCache
+import com.makentoshe.habrachan.application.common.arena.article.ArticleArena
+import com.makentoshe.habrachan.network.manager.GetArticleManager
+import com.makentoshe.habrachan.network.request.GetArticleRequest2
+import javax.inject.Inject
+import javax.inject.Provider
+
+class SourceFirstArticleArenaProvider @Inject constructor(
+    private val manager: GetArticleManager<GetArticleRequest2>,
+    private val arenaCache: ArticleArenaCache
+) : Provider<ArticleArena> {
+    override fun get(): ArticleArena {
+        return ArticleArena.Factory(manager, arenaCache).sourceFirstArena()
+    }
+}
