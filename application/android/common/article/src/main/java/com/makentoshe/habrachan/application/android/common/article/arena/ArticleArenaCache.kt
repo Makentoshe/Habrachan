1371bbdd50dbc8aec6d7fb32d7a44d109d8ab9f5
Makentoshe <mkliverout@gmail.com>
2021-08-31 11:06:55 +0300

    feat(application, android, common, article): implement ArticleArenaCache
    Entity that access ArticleArena work with cache. In this implementation cache stores in android database.

---
+++ application/android/common/article/src/main/java/com/makentoshe/habrachan/application/android/common/article/arena/ArticleArenaCache.kt
@@ -0,0 +1,57 @@
+package com.makentoshe.habrachan.application.android.common.article.arena
+
+import com.makentoshe.habrachan.application.android.analytics.Analytics
+import com.makentoshe.habrachan.application.android.analytics.LogAnalytic
+import com.makentoshe.habrachan.application.android.analytics.event.analyticEvent
+import com.makentoshe.habrachan.application.android.database.AndroidCacheDatabase
+import com.makentoshe.habrachan.application.android.database.record.ArticleAuthorRecord
+import com.makentoshe.habrachan.application.android.database.record.ArticleHubCrossRef
+import com.makentoshe.habrachan.application.android.database.record.ArticleRecord2
+import com.makentoshe.habrachan.application.android.database.record.HubRecord2
+import com.makentoshe.habrachan.application.common.arena.ArenaCache
+import com.makentoshe.habrachan.application.common.arena.ArenaStorageException
+import com.makentoshe.habrachan.functional.Result
+import com.makentoshe.habrachan.network.request.GetArticleRequest2
+import com.makentoshe.habrachan.network.response.GetArticleResponse2
+import com.makentoshe.habrachan.network.response.getArticleResponse
+import javax.inject.Inject
+
+class ArticleArenaCache @Inject constructor(
+    private val cacheDatabase: AndroidCacheDatabase
+) : ArenaCache<GetArticleRequest2, GetArticleResponse2> {
+
+    companion object : Analytics(LogAnalytic())
+
+    override fun fetch(key: GetArticleRequest2): Result<GetArticleResponse2> {
+        capture(analyticEvent { "Fetch article from cache by key: $key" })
+        return try {
+            val record = cacheDatabase.articlesDao().getByIdWithHubsAndFlows(key.articleId.articleId)
+            capture(analyticEvent { "Article(${record?.article?.articleId}) fetched from cache" })
+            if (record != null) {
+                Result.success(getArticleResponse(key, record.toArticle()))
+            } else {
+                Result.failure(ArenaStorageException("ArticleArenaCache: ArticleRecord is null"))
+            }
+        } catch (exception: Exception) {
+            capture(analyticEvent { "Couldn't fetch article: $exception" })
+            Result.failure(ArenaStorageException("ArticlesArenaCache").initCause(exception))
+        }
+    }
+
+    override fun carry(key: GetArticleRequest2, value: GetArticleResponse2) {
+        capture(analyticEvent { "Carry search articles to cache with key: $key" })
+
+        cacheDatabase.articlesDao().insert(ArticleRecord2(ArticleRecord2.UNDEFINED, value.article))
+        cacheDatabase.articleAuthorDao().insert(ArticleAuthorRecord(value.article.author))
+
+//        TODO[26.06.2021] - list of flows returns Flow(id=0, name=null, alias=null, url=https://habr.com/ru/flows//, path=/flows//, hubsCount=null) for each element
+//        value.article.flows.forEach { flow ->
+//            cacheDatabase.flowDao().insert(FlowRecord2(flow))
+//            cacheDatabase.articleFlowCrossRefDao().insert(ArticleFlowCrossRef(value.article.articleId, flow.flowId))
+//        }
+        value.article.hubs.forEach { hub ->
+            cacheDatabase.hubDao().insert(HubRecord2(hub))
+            cacheDatabase.articleHubCrossRefDao().insert(ArticleHubCrossRef(value.article.articleId, hub.hubId))
+        }
+    }
+}
