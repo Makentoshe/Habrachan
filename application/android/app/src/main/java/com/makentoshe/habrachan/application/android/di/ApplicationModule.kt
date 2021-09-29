ad0450104359c7f86c9d3c05f8ad6ff6b2be55cf
Makentoshe <mkliverout@gmail.com>
2021-09-29 14:44:15 +0300

    feat(application, android, database, articles): implement storing default search configurations in database for future using in application

--- application/android/app/src/main/java/com/makentoshe/habrachan/application/android/di/ApplicationModule.kt
+++ application/android/app/src/main/java/com/makentoshe/habrachan/application/android/di/ApplicationModule.kt
@@ -9,9 +9,12 @@ import com.makentoshe.habrachan.application.android.ExceptionHandler
 import com.makentoshe.habrachan.application.android.ExceptionHandlerImpl
 import com.makentoshe.habrachan.application.android.Launcher
 import com.makentoshe.habrachan.application.android.common.AndroidUserSession2
+import com.makentoshe.habrachan.application.android.common.R
 import com.makentoshe.habrachan.application.android.database.AndroidCacheDatabase
 import com.makentoshe.habrachan.application.android.database.UserSessionDatabase
 import com.makentoshe.habrachan.application.android.database.migration.AndroidCacheDatabaseMigration_1_2
+import com.makentoshe.habrachan.application.android.database.migration.userdatabase.userDatabaseMigrations
+import com.makentoshe.habrachan.application.android.database.record.ArticlesUserSearchRecord
 import com.makentoshe.habrachan.application.android.database.record.UserSessionRecord
 import com.makentoshe.habrachan.application.android.screen.articles.flow.navigation.ArticlesFlowScreen
 import com.makentoshe.habrachan.network.UserSession
@@ -19,7 +22,7 @@ import com.makentoshe.habrachan.network.userSession
 import toothpick.config.Module
 import toothpick.ktp.binding.bind
 
-class ApplicationModule(context: Context) : Module() {
+class ApplicationModule(private val context: Context) : Module() {
 
     private val cacheDatabase = Room.databaseBuilder(
         context, AndroidCacheDatabase::class.java, "HabrachanCache"
@@ -27,10 +30,11 @@ class ApplicationModule(context: Context) : Module() {
 
     private val userDatabase = Room.databaseBuilder(
         context, UserSessionDatabase::class.java, "HabrachanUserSensitive"
-    ).allowMainThreadQueries().build()
+    ).allowMainThreadQueries().addMigrations(*userDatabaseMigrations).build()
 
     init {
         bind<AndroidCacheDatabase>().toInstance(cacheDatabase)
+        bind<UserSessionDatabase>().toInstance(userDatabase)
         bind<Launcher>().toInstance(Launcher(ArticlesFlowScreen()))
 
         bind<ExceptionHandler>().toInstance(ExceptionHandlerImpl(context))
@@ -46,5 +50,33 @@ class ApplicationModule(context: Context) : Module() {
 
         val androidUserSession2 = AndroidUserSession2(userDatabase.userSessionDao())
         bind<AndroidUserSession2>().toInstance(androidUserSession2)
+
+        initializeDefaultUserSearches()
+    }
+
+    private fun initializeDefaultUserSearches() {
+        if (userDatabase.articlesUserSearchDao().getAll().isNotEmpty()) return
+
+        initializeDefaultAllArticlesUserSearchRecord()
+        initializeDefaultInterestingArticlesUserSearchRecord()
+        initializeDefaultTopDailyArticlesUserSearchRecord()
+    }
+
+    private fun initializeDefaultAllArticlesUserSearchRecord() {
+        val title = context.getString(R.string.articles_default_search_all)
+        val params = mapOf("a.path" to "posts/all", "a.page" to "1")
+        userDatabase.articlesUserSearchDao().insert(ArticlesUserSearchRecord(title, params, true))
+    }
+
+    private fun initializeDefaultInterestingArticlesUserSearchRecord() {
+        val title = context.getString(R.string.articles_default_search_interesting)
+        val params = mapOf("a.path" to "posts/interesting", "a.page" to "1")
+        userDatabase.articlesUserSearchDao().insert(ArticlesUserSearchRecord(title, params, true))
+    }
+
+    private fun initializeDefaultTopDailyArticlesUserSearchRecord() {
+        val title = context.getString(R.string.articles_default_search_daily_top)
+        val params = mapOf("a.path" to "top/daily", "a.page" to "1")
+        userDatabase.articlesUserSearchDao().insert(ArticlesUserSearchRecord(title, params, true))
     }
 }
