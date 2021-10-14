0b5e5854517ec095351a3966ba52edf4e0606a54
Makentoshe <mkliverout@gmail.com>
2021-10-14 17:56:38 +0300

    style(habrachan, api): simple formatting refactorings

--- application/common/arena/src/main/kotlin/com/makentoshe/habrachan/application/common/arena/ArenaCache.kt
+++ application/common/arena/src/main/kotlin/com/makentoshe/habrachan/application/common/arena/ArenaCache.kt
@@ -1,6 +1,8 @@
 package com.makentoshe.habrachan.application.common.arena
 
+import com.makentoshe.habrachan.functional.Either
 import com.makentoshe.habrachan.functional.Result
+
 /**
  * Performs main storage operation for the arenas
  */
@@ -11,6 +13,23 @@ interface ArenaCache<K, V> {
      * If could not return record by the key - returns [Result] with the [ArenaStorageException]
      */
     fun fetch(key: K): Result<V>
+
+    /**
+     * Save result to the storage.
+     */
+    fun carry(key: K, value: V)
+}
+
+/**
+ * Performs main storage operation for the arenas
+ */
+interface ArenaCache2<K, V> {
+
+    /**
+     * Fetch result from the storage.
+     * If could not return record by the key - returns [Either.Right] with the [ArenaStorageException]
+     */
+    fun fetch(key: K): Either<V, ArenaStorageException>
 
     /**
      * Save result to the storage.
