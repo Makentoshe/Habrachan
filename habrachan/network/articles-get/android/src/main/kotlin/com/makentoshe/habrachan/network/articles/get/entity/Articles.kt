4d0f46d0568af25e3df1005be830f81d86d82607
Makentoshe <mkliverout@gmail.com>
2021-10-15 01:03:16 +0300

    feat(habrachan, network): implement delegates for articles object

--- habrachan/network/articles-get/android/src/main/kotlin/com/makentoshe/habrachan/network/articles/get/entity/Articles.kt
+++ habrachan/network/articles-get/android/src/main/kotlin/com/makentoshe/habrachan/network/articles/get/entity/Articles.kt
@@ -4,18 +4,27 @@ import com.makentoshe.habrachan.delegate.requireIntReadonlyProperty
 import com.makentoshe.habrachan.delegate.requireListReadonlyProperty
 import com.makentoshe.habrachan.delegate.requireStringReadonlyProperty
 import com.makentoshe.habrachan.entity.article.Article
+import com.makentoshe.habrachan.entity.article.ArticlePropertiesDelegate
+import com.makentoshe.habrachan.functional.com.makentoshe.habrachan.AnyWithVolumeParameters
 import com.makentoshe.habrachan.functional.com.makentoshe.habrachan.delegate.optionReadonlyProperty
+import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.jsonObject
+
+data class ArticlesPropertiesDelegateImpl(
+    override val parameters: Map<String, JsonElement>,
+    private val articlePropertiesDelegateFactory: (Map<String, JsonElement>) -> ArticlePropertiesDelegate,
+) : ArticlesPropertiesDelegate, AnyWithVolumeParameters<JsonElement> {
+
+    override val articles by requireListReadonlyProperty("data") { jsonObject ->
+        jsonObject.toMap().let { Article(it, articlePropertiesDelegateFactory(it)) }
+    }
+}
 
 val Articles.pages by requireIntReadonlyProperty("pages")
 
 val Articles.sortedBy by requireStringReadonlyProperty("sorted_by")
 
 val Articles.serverTime by requireStringReadonlyProperty("server_time")
-
-val Articles.articles by requireListReadonlyProperty("data") { jsonObject ->
-    Article(jsonObject.toMap())
-}
 
 val Articles.nextPage by optionReadonlyProperty("next_page") { jsonElement ->
     ArticlesPagination(jsonElement.jsonObject.toMap())
--- habrachan/network/articles-get/mobile/src/main/kotlin/com/makentoshe/habrachan/network/articles/get/GetArticlesManagerImpl.kt
+++ habrachan/network/articles-get/mobile/src/main/kotlin/com/makentoshe/habrachan/network/articles/get/GetArticlesManagerImpl.kt
@@ -5,12 +5,28 @@ import com.makentoshe.habrachan.api.mobile.MobileHabrApi
 import com.makentoshe.habrachan.api.mobile.articles
 import com.makentoshe.habrachan.api.mobile.articles.build
 import com.makentoshe.habrachan.api.mobile.articles.filter
+import com.makentoshe.habrachan.entity.mobile.ArticleAuthorPropertiesDelegateImpl
+import com.makentoshe.habrachan.entity.mobile.ArticlePropertiesDelegateImpl
+import com.makentoshe.habrachan.network.articles.get.entity.Articles
+import com.makentoshe.habrachan.network.articles.get.entity.ArticlesPropertiesDelegateImpl
 import io.ktor.client.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
 
 class GetArticlesManagerImpl(client: HttpClient) : GetArticlesManager(client) {
 
     override fun url(request: GetArticlesRequest): HabrArticlesFilterApi {
         return MobileHabrApi.articles().filter(*request.filters).build(request.parameters)
+    }
+
+    override fun articles(json: String): Articles {
+        val parameters = Json.decodeFromString<JsonObject>(json).toMap()
+        return Articles(parameters, ArticlesPropertiesDelegateImpl(parameters) { articleParameters ->
+            ArticlePropertiesDelegateImpl(articleParameters) { articleAuthorParameters ->
+                ArticleAuthorPropertiesDelegateImpl(articleAuthorParameters)
+            }
+        })
     }
 
 }
