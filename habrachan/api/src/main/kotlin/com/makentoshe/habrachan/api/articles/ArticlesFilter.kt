a359fb6cb3be6267a05fe27f1dd87bd0e6703d78
Makentoshe <mkliverout@gmail.com>
2021-10-18 18:35:42 +0300

    feat(habrachan, api): implement several util methods and variables for ArticlesFilter
    - equals and hashCode
    - method new((String) -> String) for creating a new instance of a filter applying map function to a value
    - variable type, that returns filter type: PathArticlesFilter or QueryArticlesFilter
    - getter pair that returns ArticlesFilter as a Pair instance

--- habrachan/api/src/main/kotlin/com/makentoshe/habrachan/api/articles/ArticlesFilter.kt
+++ habrachan/api/src/main/kotlin/com/makentoshe/habrachan/api/articles/ArticlesFilter.kt
@@ -7,13 +7,23 @@ sealed class ArticlesFilter {
     abstract val key: String
     abstract val value: String
 
+    /** Creates a same filter with a new value. */
+    abstract fun new(valueMap: (String) -> String): ArticlesFilter
+
     /**
      * This filter applies to url queries.
      *
      * For example, `queryArticlesFilter("page", "39")` becomes .../?page=39.
      * */
-    data class QueryArticlesFilter(override val key: String, override val value: String): ArticlesFilter() {
-        constructor(pair: Pair<String, String>): this(pair.first, pair.second)
+    data class QueryArticlesFilter(override val key: String, override val value: String) : ArticlesFilter() {
+        constructor(pair: Pair<String, String>) : this(pair.first, pair.second)
+
+        override fun new(valueMap: (String) -> String): ArticlesFilter {
+            return QueryArticlesFilter(key, valueMap(value))
+        }
+
+        override val type: String
+            get() = "QueryArticlesFilter"
     }
 
     /**
@@ -21,12 +31,40 @@ sealed class ArticlesFilter {
      *
      * For example, `pathArticlesFilter("post", "all")` becomes .../post/all/.
      * */
-    data class PathArticlesFilter(override val key: String, override val value: String): ArticlesFilter() {
-        constructor(pair: Pair<String, String>): this(pair.first, pair.second)
+    data class PathArticlesFilter(override val key: String, override val value: String) : ArticlesFilter() {
+        constructor(pair: Pair<String, String>) : this(pair.first, pair.second)
+
+        override fun new(valueMap: (String) -> String): PathArticlesFilter {
+            return PathArticlesFilter(key, valueMap(value))
+        }
+
+        override val type: String
+            get() = "PathArticlesFilter"
     }
 
     override fun toString(): String {
         return "ArticlesFilter(key=$key, value=$value)"
+    }
+
+    val pair: Pair<String, String>
+        get() = Pair(key, value)
+
+    open val type: String
+        get() = "ArticlesFilter"
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ArticlesFilter
+
+        if (key != other.key) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return key.hashCode()
     }
 }
 
@@ -41,3 +79,5 @@ fun Array<out ArticlesFilter>.findFilter(name: String): Option<ArticlesFilter> {
 fun List<ArticlesFilter>.findFilter(name: String): Option<ArticlesFilter> {
     return Option.from(find { filter -> filter.key == name })
 }
+
+fun Array<out ArticlesFilter>.toMap() = associate { it.key to it.value }
