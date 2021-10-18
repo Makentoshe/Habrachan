a4b6caca1995d10ab3a30378a4e2db138483a9fa
Makentoshe <mkliverout@gmail.com>
2021-10-18 15:01:29 +0300

    feat(application, arena): implement EmptyArenaStorageException

---
+++ application/common/arena/src/main/kotlin/com/makentoshe/habrachan/application/common/arena/EmptyArenaStorageException.kt
@@ -0,0 +1,7 @@
+package com.makentoshe.habrachan.application.common.arena
+
+/**
+ * Thrown to indicate that the request to cache was successful, but there are no elements in database.
+ * So, we don't want to return the empty list and that return an error which contains this exception
+ * */
+class EmptyArenaStorageException(override val message: String): Throwable()
