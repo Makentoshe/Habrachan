655389fd54b5dbc50b437dba2aa537f892dd8aae
Makentoshe <mkliverout@gmail.com>
2021-10-20 18:31:35 +0300

    refactor(application, arena): update types

--- application/common/arena/src/main/kotlin/com/makentoshe/habrachan/application/common/arena/Arena3.kt
+++ application/common/arena/src/main/kotlin/com/makentoshe/habrachan/application/common/arena/Arena3.kt
@@ -1,38 +1,38 @@
 package com.makentoshe.habrachan.application.common.arena
 
-import com.makentoshe.habrachan.functional.Either
+import com.makentoshe.habrachan.functional.Either2
 
 abstract class Arena3<in K, V> {
 
     protected abstract val arenaStorage: ArenaCache3<in K, V>
 
     /** Main method for performing network operation */
-    abstract suspend fun internalSuspendFetch(key: K): Either<V, Throwable>
+    abstract suspend fun internalSuspendFetch(key: K): Either2<V, Throwable>
 
     protected suspend fun sourceFirstSuspendFetch(key: K) = internalSuspendFetch(key).fold({ sourceFetch ->
         arenaStorage.carry(key, sourceFetch)
-        Either.Left(sourceFetch)
+        Either2.Left(sourceFetch)
     }, { sourceThrowable ->
         arenaStorage.fetch(key).fold({ cacheFetch ->
-            Either.Left(cacheFetch)
+            Either2.Left(cacheFetch)
         }, { cacheThrowable ->
-            Either.Right(ArenaException(sourceException = sourceThrowable, cacheException = cacheThrowable))
+            Either2.Right(ArenaException(sourceException = sourceThrowable, cacheException = cacheThrowable))
         })
     })
 
-    protected suspend fun cacheFirstSuspendFetch(key: K): Either<V, ArenaException> {
+    protected suspend fun cacheFirstSuspendFetch(key: K): Either2<V, ArenaException> {
         return arenaStorage.fetch(key).fold({ storageFetch ->
-            Either.Left(storageFetch)
+            Either2.Left(storageFetch)
         }, { storageThrowable ->
             internalSuspendFetch(key).fold({ sourceFetch ->
                 arenaStorage.carry(key, sourceFetch)
-                Either.Left(sourceFetch)
+                Either2.Left(sourceFetch)
             }, { sourceThrowable ->
-                Either.Right(ArenaException(sourceException = sourceThrowable, cacheException = storageThrowable))
+                Either2.Right(ArenaException(sourceException = sourceThrowable, cacheException = storageThrowable))
             })
         })
     }
 
     /** Method executes network and caches operations */
-    abstract suspend fun suspendFetch(key: K): Either<V, ArenaException>
+    abstract suspend fun suspendFetch(key: K): Either2<V, ArenaException>
 }
--- application/common/arena/src/main/kotlin/com/makentoshe/habrachan/application/common/arena/ArenaCache3.kt
+++ application/common/arena/src/main/kotlin/com/makentoshe/habrachan/application/common/arena/ArenaCache3.kt
@@ -1,6 +1,6 @@
 package com.makentoshe.habrachan.application.common.arena
 
-import com.makentoshe.habrachan.functional.Either
+import com.makentoshe.habrachan.functional.Either2
 
 /**
  * Performs main storage operation for the arenas
@@ -9,9 +9,9 @@ interface ArenaCache3<K, V> {
 
     /**
      * Fetch result from the storage.
-     * If could not return record by the key - returns [Either.Right] with the [ArenaStorageException]
+     * If could not return record by the key - returns [Either2.Right] with the [ArenaStorageException]
      */
-    fun fetch(key: K): Either<V, ArenaStorageException>
+    fun fetch(key: K): Either2<V, ArenaStorageException>
 
     /**
      * Save result to the storage.
