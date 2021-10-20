a59dd29113ce03cbe723a9b593d4066b94aca985
Makentoshe <mkliverout@gmail.com>
2021-10-20 18:41:45 +0300

    feat(application, android, database): fix types

--- application/android/database/src/main/java/com/makentoshe/habrachan/application/android/database/record/userdatabase/ArticlesFilterRecord.kt
+++ application/android/database/src/main/java/com/makentoshe/habrachan/application/android/database/record/userdatabase/ArticlesFilterRecord.kt
@@ -2,8 +2,8 @@ package com.makentoshe.habrachan.application.android.database.record.userdatabas
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.makentoshe.habrachan.Option
 import com.makentoshe.habrachan.api.articles.ArticlesFilter
+import com.makentoshe.habrachan.functional.Option2
 
 @Entity
 data class ArticlesFilterRecord(
@@ -11,14 +11,14 @@ data class ArticlesFilterRecord(
 
     val value: String,
 
-    /** An article filter type. Might be [ArticlesFilter], [QueryArticlesFilter] or [PathArticlesFilter]*/
+    /** An article filter type. Might be ArticlesFilter, QueryArticlesFilter or PathArticlesFilter */
     val internalType: String,
 
     @PrimaryKey
     val keyValuePair: String = "$internalType(type=$internalType, $key=$value)"
 )
 
-fun ArticlesFilterRecord.toArticlesFilter(): Option<ArticlesFilter> = Option.from(
+fun ArticlesFilterRecord.toArticlesFilter(): Option2<ArticlesFilter> = Option2.from(
     when (internalType) {
         "PathArticlesFilter" -> ArticlesFilter.PathArticlesFilter(key, value)
         "QueryArticlesFilter" -> ArticlesFilter.QueryArticlesFilter(key, value)
