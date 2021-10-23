731a2ab8a469a5e3799c296ffebe28c3453d8848
Makentoshe <mkliverout@gmail.com>
2021-10-23 11:34:16 +0300

    feat(habrachan): implement new requireReadonlyProperty function

--- habrachan/src/main/kotlin/com/makentoshe/habrachan/delegate/RequireProperty.kt
+++ habrachan/src/main/kotlin/com/makentoshe/habrachan/delegate/RequireProperty.kt
@@ -7,9 +7,9 @@ import kotlin.properties.ReadOnlyProperty
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-abstract class Require2ReadonlyProperty<Owner, Type>: ReadOnlyProperty<Owner, Require2<Type>>
+abstract class Require2ReadonlyProperty<Owner, Type> : ReadOnlyProperty<Owner, Require2<Type>>
 
-abstract class Require2ReadWriteProperty<Owner, Type>: ReadWriteProperty<Owner, Require2<Type>>
+abstract class Require2ReadWriteProperty<Owner, Type> : ReadWriteProperty<Owner, Require2<Type>>
 
 class JsonElementRequire2ReadonlyProperty<Type>(
     private vararg val keys: String,
@@ -22,11 +22,19 @@ class JsonElementRequire2ReadonlyProperty<Type>(
     } catch (exception: IllegalArgumentException) {
         val message = "IllegalArgumentException(message=${exception.message}, parameters=${thisRef.parameters})"
         throw IllegalArgumentException(message, exception.cause)
+    } catch (exception: NullPointerException) { // if declared keys more that presented
+        val message = "IllegalArgumentException(message=${exception.message}, parameters=${thisRef.parameters})"
+        throw IllegalArgumentException(message, exception.cause)
+    } catch (exception: Exception) {
+        throw Exception("Anus")
     }
 
 }
 
-fun <Type> requireReadonlyProperty(vararg keys: String, map: (JsonElement) -> Type): JsonElementRequire2ReadonlyProperty<Type> {
+fun <Type> requireReadonlyProperty(
+    vararg keys: String,
+    map: (JsonElement) -> Type
+): JsonElementRequire2ReadonlyProperty<Type> {
     return JsonElementRequire2ReadonlyProperty(*keys, map = map)
 }
 
@@ -38,10 +46,6 @@ fun requireBooleanReadonlyProperty(vararg keys: String): JsonElementRequire2Read
     return JsonElementRequire2ReadonlyProperty(*keys) { jsonElement -> jsonElement.jsonPrimitive.boolean }
 }
 
-fun requireIntReadonlyProperty(vararg keys: String): JsonElementRequire2ReadonlyProperty<Int> {
-    return JsonElementRequire2ReadonlyProperty(*keys) { jsonElement -> jsonElement.jsonPrimitive.int }
-}
-
 fun requireFloatReadonlyProperty(vararg keys: String): JsonElementRequire2ReadonlyProperty<Float> {
     return JsonElementRequire2ReadonlyProperty(*keys) { jsonElement -> jsonElement.jsonPrimitive.float }
 }
@@ -50,6 +54,47 @@ fun requireDoubleReadonlyProperty(vararg keys: String): JsonElementRequire2Reado
     return JsonElementRequire2ReadonlyProperty(*keys) { jsonElement -> jsonElement.jsonPrimitive.double }
 }
 
-fun <Type> requireListReadonlyProperty(vararg keys: String, mapElement: (JsonObject) -> Type): JsonElementRequire2ReadonlyProperty<List<Type>> {
-    return JsonElementRequire2ReadonlyProperty(*keys) { jsonElement -> jsonElement.jsonArray.filterIsInstance<JsonObject>().map(mapElement) }
+fun <Type> requireListReadonlyProperty(
+    vararg keys: String,
+    mapElement: (JsonObject) -> Type
+): JsonElementRequire2ReadonlyProperty<List<Type>> {
+    return JsonElementRequire2ReadonlyProperty(*keys) { jsonElement ->
+        jsonElement.jsonArray.filterIsInstance<JsonObject>().map(mapElement)
+    }
+}
+
+fun <Type> requireReadonlyProperty(
+    vararg mappers: PropertyMapper<Type>
+) : Require2ReadonlyProperty<AnyWithVolumeParameters<JsonElement>, Type> {
+    return JsonElementRequire2ReadonlyProperty2(*mappers)
+}
+
+interface PropertyMapper<Type> {
+    val keys: Array<out String>
+    val map: (JsonElement) -> Type?
+}
+
+fun <Type> propertyMapper(vararg keys: String, map: (JsonElement) -> Type?) = object : PropertyMapper<Type> {
+    override val keys: Array<out String> = keys
+    override val map: (JsonElement) -> Type? = map
+}
+
+fun intPropertyMapper(vararg keys: String) = object: PropertyMapper<Int> {
+    override val keys: Array<out String> = keys
+    override val map: (JsonElement) -> Int? = { jsonElement -> jsonElement.jsonPrimitive.int }
+}
+
+class JsonElementRequire2ReadonlyProperty2<Type>(
+    private vararg val propertyMappers: PropertyMapper<Type>
+) : Require2ReadonlyProperty<AnyWithVolumeParameters<JsonElement>, Type>() {
+
+    override fun getValue(thisRef: AnyWithVolumeParameters<JsonElement>, property: KProperty<*>): Require2<Type> {
+        return Require2(propertyMappers.mapNotNull { mapper -> getMapperValue(mapper, thisRef) }.firstOrNull())
+    }
+
+    private fun getMapperValue(mapper: PropertyMapper<Type>, thisRef: AnyWithVolumeParameters<JsonElement>): Type? {
+        val initial = mapper.keys.firstOrNull()?.let(thisRef.parameters::get)
+        val jsonElement = mapper.keys.drop(1).fold(initial) { jsonElement, key -> jsonElement?.jsonObject?.get(key) }
+        return if (jsonElement == null) null else mapper.map(jsonElement)
+    }
 }
