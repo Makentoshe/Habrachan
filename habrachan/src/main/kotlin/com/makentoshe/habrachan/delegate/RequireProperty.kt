bcf347524967a613c10c3d1d031c765a4fc6c211
Makentoshe <mkliverout@gmail.com>
2021-10-29 12:29:04 +0300

    feat(habrachan): update delegates and common types

--- habrachan/src/main/kotlin/com/makentoshe/habrachan/delegate/RequireProperty.kt
+++ habrachan/src/main/kotlin/com/makentoshe/habrachan/delegate/RequireProperty.kt
@@ -50,10 +50,6 @@ fun requireFloatReadonlyProperty(vararg keys: String): JsonElementRequire2Readon
     return JsonElementRequire2ReadonlyProperty(*keys) { jsonElement -> jsonElement.jsonPrimitive.float }
 }
 
-fun requireDoubleReadonlyProperty(vararg keys: String): JsonElementRequire2ReadonlyProperty<Double> {
-    return JsonElementRequire2ReadonlyProperty(*keys) { jsonElement -> jsonElement.jsonPrimitive.double }
-}
-
 fun <Type> requireListReadonlyProperty(
     vararg keys: String,
     mapElement: (JsonObject) -> Type
@@ -64,35 +60,40 @@ fun <Type> requireListReadonlyProperty(
 }
 
 fun <Type> requireReadonlyProperty(
-    vararg mappers: PropertyMapper<Type>
+    vararg mappers: JsonElementPropertyMapper<Type>
 ) : Require2ReadonlyProperty<AnyWithVolumeParameters<JsonElement>, Type> {
     return JsonElementRequire2ReadonlyProperty2(*mappers)
 }
 
-interface PropertyMapper<Type> {
+interface JsonElementPropertyMapper<Type> {
     val keys: Array<out String>
     val map: (JsonElement) -> Type?
 }
 
-fun <Type> propertyMapper(vararg keys: String, map: (JsonElement) -> Type?) = object : PropertyMapper<Type> {
+fun <Type> propertyMapper(vararg keys: String, map: (JsonElement) -> Type?) = object : JsonElementPropertyMapper<Type> {
     override val keys: Array<out String> = keys
     override val map: (JsonElement) -> Type? = map
 }
 
-fun intPropertyMapper(vararg keys: String) = object: PropertyMapper<Int> {
+fun intPropertyMapper(vararg keys: String) = object: JsonElementPropertyMapper<Int> {
     override val keys: Array<out String> = keys
     override val map: (JsonElement) -> Int? = { jsonElement -> jsonElement.jsonPrimitive.int }
 }
 
+fun stringPropertyMapper(vararg keys: String) = object : JsonElementPropertyMapper<String> {
+    override val keys: Array<out String> = keys
+    override val map: (JsonElement) -> String? = { jsonElement -> jsonElement.jsonPrimitive.contentOrNull }
+}
+
 class JsonElementRequire2ReadonlyProperty2<Type>(
-    private vararg val propertyMappers: PropertyMapper<Type>
+    private vararg val jsonElementPropertyMappers: JsonElementPropertyMapper<Type>
 ) : Require2ReadonlyProperty<AnyWithVolumeParameters<JsonElement>, Type>() {
 
     override fun getValue(thisRef: AnyWithVolumeParameters<JsonElement>, property: KProperty<*>): Require2<Type> {
-        return Require2(propertyMappers.mapNotNull { mapper -> getMapperValue(mapper, thisRef) }.firstOrNull())
+        return Require2(jsonElementPropertyMappers.mapNotNull { mapper -> getMapperValue(mapper, thisRef) }.firstOrNull())
     }
 
-    private fun getMapperValue(mapper: PropertyMapper<Type>, thisRef: AnyWithVolumeParameters<JsonElement>): Type? {
+    private fun getMapperValue(mapper: JsonElementPropertyMapper<Type>, thisRef: AnyWithVolumeParameters<JsonElement>): Type? {
         val initial = mapper.keys.firstOrNull()?.let(thisRef.parameters::get)
         val jsonElement = mapper.keys.drop(1).fold(initial) { jsonElement, key -> jsonElement?.jsonObject?.get(key) }
         return if (jsonElement == null) null else mapper.map(jsonElement)
---
+++ habrachan/src/main/kotlin/com/makentoshe/habrachan/delegate/option/StringOption2ReadonlyProperty2.kt
@@ -0,0 +1,31 @@
+package com.makentoshe.habrachan.delegate.option
+
+import com.makentoshe.habrachan.AnyWithFlatParameters
+import com.makentoshe.habrachan.delegate.OptionReadonlyProperty
+import com.makentoshe.habrachan.functional.Option2
+import kotlin.reflect.KProperty
+
+class StringOption2ReadonlyProperty<Type>(
+    private vararg val keys: String,
+    private val map: (String) -> Type
+) : OptionReadonlyProperty<AnyWithFlatParameters, Type>() {
+
+    override fun getValue(thisRef: AnyWithFlatParameters, property: KProperty<*>): Option2<Type> = try {
+        Option2.from(thisRef.parameters[keys.joinToString(".")]?.let { map(it) })
+    } catch (exception: IllegalArgumentException) {
+        val message = "IllegalArgumentException(message=${exception.message}, parameters=${thisRef.parameters})"
+        throw IllegalArgumentException(message, exception.cause)
+    } catch (exception: NullPointerException) { // if declared keys more that presented
+        val message = "IllegalArgumentException(message=${exception.message}, parameters=${thisRef.parameters})"
+        throw IllegalArgumentException(message, exception.cause)
+    } catch (exception: Exception) {
+        throw Exception("Anus")
+    }
+
+}
+
+fun <Type> optionReadonlyProperty(
+    vararg keys: String, map: (String) -> Type
+): StringOption2ReadonlyProperty<Type> {
+    return StringOption2ReadonlyProperty(*keys, map = map)
+}
