7a69ccd01ec492522581dd2a5329bf1c1f7f48a6
Makentoshe <mkliverout@gmail.com>
2021-11-08 22:03:37 +0300

    feat(network, login, android): move LoginSession entity to another package

--- habrachan/network/login/android/src/main/kotlin/com/makentoshe/habrachan/network/login/LoginResponse.kt
+++ habrachan/network/login/android/src/main/kotlin/com/makentoshe/habrachan/network/login/LoginResponse.kt
@@ -1,3 +1,5 @@
 package com.makentoshe.habrachan.network.login
 
+import com.makentoshe.habrachan.network.login.entity.LoginSession
+
 data class LoginResponse(val request: GetLoginRequest, val loginSession: LoginSession)
