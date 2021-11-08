67836de4cb8094fe40eca5e2002ff1c1eb7a255f
Makentoshe <mkliverout@gmail.com>
2021-11-08 21:55:49 +0300

    feat(api): add cookies field for AdditionalRequestParameters

--- habrachan/api/src/main/kotlin/com/makentoshe/habrachan/api/AdditionalRequestParameters.kt
+++ habrachan/api/src/main/kotlin/com/makentoshe/habrachan/api/AdditionalRequestParameters.kt
@@ -3,5 +3,6 @@ package com.makentoshe.habrachan.api
 data class AdditionalRequestParameters(
     val headers: Map<String, String> = emptyMap(),
     val queries: Map<String, String> = emptyMap(),
+    val cookies: Map<String, String> = emptyMap(),
 )
 
