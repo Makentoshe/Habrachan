93c85fa3be0efb035f40dd96bc7d29b89d5d9a54
Makentoshe <mkliverout@gmail.com>
2021-11-08 22:08:38 +0300

    feat(application, android, app): implement UserModule which creates and controls dependencies for storing and working with user credentials

--- application/android/app/src/main/java/com/makentoshe/habrachan/application/android/di/UserModule.kt
+++ application/android/app/src/main/java/com/makentoshe/habrachan/application/android/di/UserModule.kt
@@ -5,18 +5,24 @@ import android.content.Context
 import android.provider.Settings
 import androidx.security.crypto.EncryptedFile
 import androidx.security.crypto.MasterKey
+import com.makentoshe.habrachan.BuildConfig
+import com.makentoshe.habrachan.application.android.analytics.Analytics
+import com.makentoshe.habrachan.application.android.analytics.LogAnalytic
+import com.makentoshe.habrachan.application.android.analytics.event.analyticEventAddrless
 import com.makentoshe.habrachan.application.android.common.BuildVersionProvider
 import com.makentoshe.habrachan.application.android.common.usersession.*
+import com.makentoshe.habrachan.functional.toRequire2
 import toothpick.config.Module
 import toothpick.ktp.binding.bind
 import kotlin.random.Random
 
 class UserModule(context: Context, private val buildVersionProvider: BuildVersionProvider) : Module() {
 
+    companion object : Analytics(LogAnalytic())
+
     init {
-        val controller = buildController(context)
+        val controller = buildController(context).also(::installController)
         bind<AndroidUserSessionProvider>().toInstance(controller)
-        bind<AndroidUserSessionController>().toInstance(controller)
         bind<AndroidUserSessionController>().toInstance(controller)
     }
 
@@ -37,5 +43,17 @@ class UserModule(context: Context, private val buildVersionProvider: BuildVersio
     } else {
         val seed = Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID).hashCode()
         Random(seed).nextBytes(16).decodeToString()
+    }
+
+    private fun installController(controller: AndroidUserSessionController) = controller.accept {
+        // if requireable params weren't installed, the old value will be null
+        if (old != null) return@accept capture(analyticEventAddrless { "AndroidUserSession was already installed" })
+
+        this.client = ClientId(BuildConfig.CLIENT_KEY).toRequire2()
+        this.api = ClientApi(BuildConfig.API_KEY).toRequire2()
+
+        capture(analyticEventAddrless {
+            "Install AndroidUserSession(client=${this@accept.client.nullableValue}, api=${this@accept.api.nullableValue}"
+        })
     }
 }
