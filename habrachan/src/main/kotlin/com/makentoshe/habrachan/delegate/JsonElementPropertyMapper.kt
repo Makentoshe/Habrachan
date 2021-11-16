994790160ffcdbf2fd1e0ac676c53a6affa6120a
Makentoshe <mkliverout@gmail.com>
2021-11-16 16:51:17 +0300

    feat: add several new property mappers

---
+++ habrachan/src/main/kotlin/com/makentoshe/habrachan/delegate/JsonElementPropertyMapper.kt
@@ -0,0 +1,33 @@
+package com.makentoshe.habrachan.delegate
+
+import kotlinx.serialization.json.*
+
+interface JsonElementPropertyMapper<Type> {
+    val keys: Array<out String>
+    val map: (JsonElement) -> Type?
+}
+
+fun <Type> propertyMapper(vararg keys: String, map: (JsonElement) -> Type?) = object : JsonElementPropertyMapper<Type> {
+    override val keys: Array<out String> = keys
+    override val map: (JsonElement) -> Type? = map
+}
+
+fun intPropertyMapper(vararg keys: String) = object: JsonElementPropertyMapper<Int> {
+    override val keys: Array<out String> = keys
+    override val map: (JsonElement) -> Int? = { jsonElement -> jsonElement.jsonPrimitive.int }
+}
+
+fun stringPropertyMapper(vararg keys: String) = object : JsonElementPropertyMapper<String> {
+    override val keys: Array<out String> = keys
+    override val map: (JsonElement) -> String? = { jsonElement -> jsonElement.jsonPrimitive.contentOrNull }
+}
+
+fun floatPropertyMapper(vararg keys: String) = object : JsonElementPropertyMapper<Float> {
+    override val keys: Array<out String> = keys
+    override val map: (JsonElement) -> Float? = { jsonElement -> jsonElement.jsonPrimitive.floatOrNull }
+}
+
+fun booleanPropertyMapper(vararg keys: String) = object : JsonElementPropertyMapper<Boolean> {
+    override val keys: Array<out String> = keys
+    override val map: (JsonElement) -> Boolean? = { jsonElement -> jsonElement.jsonPrimitive.booleanOrNull }
+}
